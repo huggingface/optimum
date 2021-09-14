f51d5580cc4f3fcd57b833eef49a469aa732cbb8
Morgan <funtowiczmo@gmail.com>
2021-09-14 11:52:59 +0200

    Added project URL.

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     keywords="transformers, quantization, pruning, training, ipu",
-    url="",
+    url="hf.co/hardware",
     author="HuggingFace Inc. Special Ops Team",
     author_email="hardware@huggingface.co",
     license="Apache",
