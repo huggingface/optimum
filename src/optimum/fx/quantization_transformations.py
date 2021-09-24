69a086cd06c35c57f5a31b8c8ab0101b5e0ed376
Michael Benayoun <mickbenayoun@gmail.com>
2021-09-24 12:23:35 +0200

    implemented broadcasting to every add operator in the graph as torch.ops.quantized.add does not support broadcasting

--- src/optimum/fx/quantization_transformations.py
+++ src/optimum/fx/quantization_transformations.py
@@ -1,12 +1,13 @@
 import operator
-import functools
-from typing import Callable
+from typing import Any, List
 
 import torch
-from torch.fx import GraphModule
+from torch.fx import Interpreter, GraphModule, Node
+
+from transformers.utils.fx_transformations import transformation, compose_transformations
 
 
-def change_truediv_to_mul_when_possible(gm: GraphModule) -> GraphModule:
+def change_truediv_to_mul_when_possible_(gm: GraphModule, lint_and_recompile: bool = True):
     """
     Transforms truediv nodes by multiplication of the inverse when the denominator is static.
     For example, that is sometimes the case for the scaling factor in attention layers.
@@ -18,12 +19,15 @@ def change_truediv_to_mul_when_possible(gm: GraphModule) -> GraphModule:
             if not isinstance(y, torch.fx.Node):
                 node.target = operator.mul
                 node.args = (x, 1 / y)
-    graph.lint()
-    gm.recompile()
-    return gm
+
+    if lint_and_recompile:
+        graph.lint()
+        gm.recompile()
 
 
-def change_attention_mask_value(gm: GraphModule, initial_value: int = -10000, final_value: int = -20) -> GraphModule:
+def change_attention_mask_value_(
+    gm: GraphModule, initial_value: int = -10000, final_value: int = -20, lint_and_recompile: bool = True
+):
     """Changes the attention mask initial value (default is -10000) to some smaller value."""
     graph = gm.graph
     for node in graph.nodes:
@@ -36,73 +40,139 @@ def change_attention_mask_value(gm: GraphModule, initial_value: int = -10000, fi
                     new_args.append(arg)
             node.args = tuple(new_args)
             break
-    graph.lint()
+
+    if lint_and_recompile:
+        graph.lint()
+        gm.recompile()
+
+
+class BroadcastAddOps(Interpreter):
+    """
+    An Interpreter that broadcasts all the operands of operator.add to make them match in terms of shapes.
+    This is useful when quantizing a model as torch.ops.quantized.add does not support broadcasting yet.
+    """
+
+    def insert_broadcasting_op(
+        self, input_to_broadcast: Node, broadcast_size: List[int], other_operand_size_node: Node
+    ):
+        """
+        Inserts a torch.broadcast_to op to broadcast input_to_broadcast on dimensions where broadcast_size is not -1 by
+        retrieving the size of the other operand node on these dimensions.
+        """
+        graph = self.module.graph
+        symbolic_broadcast_size = []
+        node_to_insert_after = None
+        for i, dim in enumerate(broadcast_size):
+            if dim != -1:
+                with graph.inserting_after(other_operand_size_node):
+                    get_ith_item_node = graph.call_function(operator.getitem, args=(other_operand_size_node, i))
+                    if node_to_insert_after is None:
+                        node_to_insert_after = get_ith_item_node
+                    symbolic_broadcast_size.append(get_ith_item_node)
+            else:
+                symbolic_broadcast_size.append(-1)
+
+        with graph.inserting_after(node_to_insert_after):
+            broadcast_op_node = graph.call_function(
+                torch.broadcast_to, args=(input_to_broadcast, tuple(symbolic_broadcast_size))
+            )
+
+        return broadcast_op_node
+
+    def run_node(self, n: Node) -> Any:
+        result = super().run_node(n)
+        args, kwargs = self.fetch_args_kwargs_from_env(n)
+        graph = self.module.graph
+
+        if n.target is operator.add:
+            a, b = args
+            shape_a = a.size()
+            shape_b = b.size()
+            rank_a = len(shape_a)
+            rank_b = len(shape_b)
+            broadcast_size_a = []
+            broadcast_size_b = []
+            min_rank = min(rank_a, rank_b)
+            max_rank = max(rank_a, rank_b)
+            for i in range(min_rank - 1, -1, -1):
+                dim_a = shape_a[i]
+                dim_b = shape_b[i]
+                if dim_a == dim_b:
+                    broadcast_size_a.insert(0, -1)
+                    broadcast_size_b.insert(0, -1)
+                elif dim_a == 1:
+                    broadcast_size_a.insert(0, dim_b)
+                    broadcast_size_b.insert(0, -1)
+                elif dim_b == 1:
+                    broadcast_size_a.insert(0, -1)
+                    broadcast_size_b.insert(0, dim_a)
+                else:
+                    # We should never encounter that case.
+                    continue
+
+            broadcast_size_a = list(shape_b[: max_rank - rank_a]) + broadcast_size_a
+            broadcast_size_b = list(shape_a[: max_rank - rank_b]) + broadcast_size_b
+
+            a_needs_broadcasting = any(dim != -1 for dim in broadcast_size_a)
+            b_needs_broadcasting = any(dim != -1 for dim in broadcast_size_b)
+
+            if a_needs_broadcasting or b_needs_broadcasting:
+                with graph.inserting_before(n):
+                    if b_needs_broadcasting:
+                        size_a = graph.call_method("size", args=(n.args[0],))
+                    if a_needs_broadcasting:
+                        size_b = graph.call_method("size", args=(n.args[1],))
+
+                new_node_args = []
+
+                if a_needs_broadcasting:
+                    broadcast_op_node = self.insert_broadcasting_op(n.args[0], broadcast_size_a, size_b)
+                    new_node_args.append(broadcast_op_node)
+                else:
+                    new_node_args.append(n.args[0])
+
+                if b_needs_broadcasting:
+                    broadcast_op_node = self.insert_broadcasting_op(n.args[1], broadcast_size_b, size_a)
+                    new_node_args.append(broadcast_op_node)
+                else:
+                    new_node_args.append(n.args[1])
+
+                n.args = tuple(new_node_args)
+
+        return result
+
+
+@transformation
+def broadcast_add(gm: GraphModule) -> GraphModule:
+    """Broadcasts operands of operator.add to make them match in terms of shapes."""
+    broadcast_add_interpreter = BroadcastAddOps(gm)
+    broadcast_add_interpreter.run(*(gm.dummy_inputs.values()))
+    gm.graph.lint()
     gm.recompile()
     return gm
 
 
-def broadcast_attention_mask(gm: GraphModule) -> GraphModule:
+def remove_dequantize_after_getitem_on_size_(gm: GraphModule, lint_and_recompile: bool = True):
     """
-    Broadcasts attention_mask to match the shape of attention_scores as broadcasting is not
-    supported for quantized add operator in PyTorch.
+    Removes .dequantize() ops after operator.getitem that were applied to the result of .size().
+    This happens when quantizing a model: the tracer will insert dequantize ops, but because .size() returns
+    integers, applying dequantize to this will result in a RuntimeError.
     """
     graph = gm.graph
-    attention_mask = None
-    # TODO: find a more robust way of finding the attention_mask node.
     for node in graph.nodes:
-        if (
-            node.target in [torch.mul, torch.ops.quantized.mul, operator.mul]
-            and len(node.args) > 1
-            and node.args[1] < 0
-        ):
-            attention_mask = node
-            break
-    if attention_mask is None:
-        print("Could not find attention_mask to broadcast.")
-        return
-    for node in graph.nodes:
-        if node.target == torch.ops.quantized.add and attention_mask in node.args:
-            attention_scores, mask, *rest = node.args
-            with graph.inserting_before(node):
-                broadcasted_mask = graph.call_function(
-                    torch.broadcast_to, args=(mask, graph.call_method("size", args=(attention_scores, )))
-                )
-                new_args = []
-                for arg in node.args:
-                    if arg is mask:
-                        new_args.append(broadcasted_mask)
-                    else:
-                        new_args.append(arg)
-                node.args = tuple(new_args)
-    graph.lint()
-    gm.recompile()
-    return gm
+        if node.op == "call_method" and node.target == "dequantize":
+            parent = node.args[0]
+            if parent.op == "call_function" and parent.target == operator.getitem:
+                grandparent = parent.args[0]
+                if grandparent.op == "call_method" and grandparent.target == "size":
+                    node.replace_all_uses_with(parent)
+                    graph.erase_node(node)
+    if lint_and_recompile:
+        graph.lint()
+        gm.recompile()
 
 
-def broadcast_nonorm_bias(gm: GraphModule) -> GraphModule:
-    """
-    Broadcasts the NoNorm bias to match the shape of the scaled input as broadcasting is not
-    supported for quantized add.
-    """
-    graph = gm.graph
-    for node in graph.nodes:
-        if node.target == torch.ops.quantized.mul:
-            users = list(node.users.keys())
-            if len(users) == 1 and users[0].target == torch.ops.quantized.add:
-                add_node = users[0]
-                with graph.inserting_before(add_node):
-                    broadcasted_bias = graph.call_function(
-                        torch.broadcast_to,
-                        args=(add_node.args[1], graph.call_method("size", args=(add_node.args[0],))),
-                    )
-                    new_args = add_node.args[:1] + (broadcasted_bias,) + add_node.args[2:]
-                    add_node.args = tuple(new_args)
-
-    graph.lint()
-    gm.recompile()
-    return gm
-
-
-def compose_transformations(*args: Callable[[GraphModule], GraphModule]) -> GraphModule:
-    """Helper function that allows you to compose transformations together."""
-    return functools.reduce(lambda f, g: lambda gm: f(g(gm)), args, lambda x: x)
+pre_quantization_transformations = broadcast_add
+post_quantization_transformations = compose_transformations(
+    change_attention_mask_value_, remove_dequantize_after_getitem_on_size_
+)
