fixed the output inconsistent issue in custom loss calculation.

examples/inc/pytorch/text-classification/run_glue.py
@@ -42,9 +42,21 @@ from transformers import (
 )
 from transformers.trainer_utils import get_last_checkpoint
 from transformers.utils import check_min_version
+from transformers.utils.fx import symbolic_trace
 
 import yaml
-from optimum.intel.neural_compressor.trainer_inc import IncTrainer
+from optimum.intel.neural_compressor import (
+    IncOptimizer,
+    IncPruner,
+    IncDistillation,
+    IncPruningConfig,
+    IncQuantizationConfig,
+    IncDistillationConfig,
+    IncQuantizationMode,
+    IncQuantizer,
+    IncTrainer,
+)
+from optimum.intel.neural_compressor.quantization import IncQuantizedModelForSequenceClassification
 from optimum.intel.neural_compressor.utils import CONFIG_NAME
 
 import functools
@@ -536,6 +548,14 @@ def main():
         data_collator = None
         
     if optim_args.distillation:
+        class BertModelforLogitsOutputOnly(torch.nn.Module):
+            def __init__(self, model):
+                super(BertModelforLogitsOutputOnly, self).__init__()
+                self.model = model
+            def forward(self, *args, **kwargs):
+                output = self.model(*args, **kwargs)
+                return output['logits']
+
         teacher_config = AutoConfig.from_pretrained(optim_args.teacher_model_name_or_path, \
                             num_labels=num_labels, finetuning_task=data_args.task_name)
         teacher_tokenizer = AutoTokenizer.from_pretrained(optim_args.teacher_model_name_or_path, \
@@ -547,6 +567,7 @@ def main():
             from_tf=bool(".ckpt" in optim_args.teacher_model_name_or_path),
             config=teacher_config,
         )
+        teacher_model = BertModelforLogitsOutputOnly(teacher_model)
         teacher_model.to(training_args.device)
         
         # prepare datasets for teacher model
@@ -585,7 +606,7 @@ def main():
                     for step, batch in enumerate(train_dataloader):
                         dict_tensor_to_model_device(batch, teacher_model)
                         outputs = teacher_model(**batch)
-                        teacher_logits += [x for x in outputs['logits'].cpu().numpy()]
+                        teacher_logits += [x for x in outputs.cpu().numpy()]
                     np.save(npy_file, np.array(teacher_logits))
                 return train_dataset.add_column('teacher_logits', teacher_logits)
             with torch.no_grad():
@@ -665,6 +686,7 @@ def main():
 
     quantizer = None
     pruner = None
+    distiller = None
 
     if not optim_args.quantize and not optim_args.prune:
         raise ValueError("quantize and prune are both set to False.")
@@ -674,8 +696,6 @@ def main():
     default_config = os.path.join(os.path.abspath(os.path.join(__file__, os.path.pardir, os.path.pardir)), "config")
 
     if optim_args.quantize:
-
-        from optimum.intel.neural_compressor import IncQuantizationConfig, IncQuantizationMode, IncQuantizer
 
         if not training_args.do_eval:
             raise ValueError("do_eval must be set to True for quantization.")
@@ -703,7 +723,6 @@ def main():
         # torch FX used for post-training quantization and quantization aware training
         # dynamic quantization will be added when torch FX is more mature
         if q8_config.get_config("quantization.approach") != IncQuantizationMode.DYNAMIC.value:
-            from transformers.utils.fx import symbolic_trace
 
             if not training_args.do_train:
                 raise ValueError("do_train must be set to True for static and aware training quantization.")
@@ -731,11 +750,9 @@ def main():
         inc_quantizer = IncQuantizer(
             model, q8_config, eval_func=eval_func, train_func=train_func, calib_dataloader=calib_dataloader
         )
-        quantizer = inc_quantizer.fit()
+        quantizer = inc_quantizer.init()
 
     if optim_args.prune:
-
-        from optimum.intel.neural_compressor import IncPruner, IncPruningConfig
 
         if not training_args.do_train:
             raise ValueError("do_train must be set to True for pruning.")
@@ -770,40 +787,36 @@ def main():
         inc_pruner = IncPruner(model, pruning_config, eval_func=eval_func, train_func=train_func)
 
         # Creation Pruning object used for IncTrainer training loop
-        pruner = inc_pruner.fit()
+        pruner = inc_pruner.init()
+    
+    if optim_args.distillation:
 
-    from neural_compressor.experimental import common
-    from neural_compressor.experimental.scheduler import Scheduler
+        if not training_args.do_train:
+            raise ValueError("do_train must be set to True for distillation.")
 
-    scheduler = Scheduler()
-    scheduler.model = common.Model(model)
+        distillation_config = IncDistillationConfig.from_pretrained(
+            optim_args.distillation_config if optim_args.distillation_config is not None else default_config,
+            config_file_name="distillation.yml",
+            cache_dir=model_args.cache_dir,
+        )
 
+        inc_distiller = IncDistillation(model, teacher_model, distillation_config, 
+                                        eval_func=eval_func, train_func=train_func)
+
+        # Creation Distillation object used for IncTrainer training loop
+        distiller = inc_distiller.init()
+
+    components = []
+    for component in [quantizer, pruner, distiller]:
+        if component is not None:
+            components.append(component)
+    inc_optimizer = IncOptimizer(model, components, eval_func=eval_func, train_func=train_func, 
+                                 one_shot_optimization=optim_args.one_shot_optimization)
     agent = pruner
     if optim_args.one_shot_optimization:
-        criterion = None
-        if optim_args.distillation:
-            from neural_compressor.experimental.common.criterion import PyTorchKnowledgeDistillationLoss
-            criterion = PyTorchKnowledgeDistillationLoss(
-                                    temperature=optim_args.temperature,
-                                    loss_types=optim_args.loss_types,
-                                    loss_weights=optim_args.loss_weights)
-            criterion.teacher_model = teacher_model
-        if optim_args.quantize:
-            agent = scheduler.combine(*[pruner, quantizer])
-            agent.train_func = train_func
-            agent.eval_func = eval_func
-            print(agent)
-        if criterion:
-            agent.criterion = criterion
-        scheduler.append(agent)
-    else:
-        if pruner is not None:
-            scheduler.append(pruner)
-
-        if quantizer is not None:
-            scheduler.append(quantizer)
-
-    opt_model = scheduler()
+        agent = inc_optimizer.scheduler.components[0]
+        
+    opt_model = inc_optimizer.fit()
 
     _, sparsity = opt_model.report_sparsity()
     result_opt_model = take_eval_steps(opt_model.model, trainer, metric_name, save_metrics=True)
@@ -818,7 +831,6 @@ def main():
     )
 
     if optim_args.quantize and optim_args.verify_loading:
-        from optimum.intel.neural_compressor.quantization import IncQuantizedModelForSequenceClassification
 
         # Load the model obtained after Intel Neural Compressor (INC) quantization
         loaded_model = IncQuantizedModelForSequenceClassification.from_pretrained(