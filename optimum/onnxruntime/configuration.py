c416f295cad226f1a47652baeb9ef6865d53e664
Ella Charlaix <ella@huggingface.co>
2022-01-13 19:37:24 +0100

    Add missing import

--- optimum/onnxruntime/configuration.py
+++ optimum/onnxruntime/configuration.py
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from typing import Any, Dict, Tuple, Union
+from typing import Any, Dict, Optional, Tuple, Union
 
 from ..configuration_utils import BaseConfig
 
