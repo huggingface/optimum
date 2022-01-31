e5f33e75cfd6c17ba2643a399b123e062a47ab7e
Ella Charlaix <ella@huggingface.co>
2022-01-31 12:14:32 +0100

    Remove sparsity calculation as the latter is incompatible with quantization

--- examples/inc/pytorch/text-classification/run_glue.py
+++ examples/inc/pytorch/text-classification/run_glue.py
@@ -676,8 +676,6 @@ def main():
 
     inc_optimizer = IncOptimizer(model, quantizer=quantizer, pruner=pruner)
     opt_model = inc_optimizer.fit()
-
-    _, sparsity = opt_model.report_sparsity()
     result_opt_model = take_eval_steps(opt_model.model, trainer, metric_name, save_metrics=True)
 
     trainer.save_model(training_args.output_dir)
@@ -685,8 +683,8 @@ def main():
         yaml.dump(opt_model.tune_cfg, f, default_flow_style=False)
 
     logger.info(
-        f"Optimized model with final sparsity of {sparsity} and {metric_name} of {result_opt_model} saved to: "
-        f"{training_args.output_dir}. Original model had an {metric_name} of {result_baseline_model}"
+        f"Optimized model with {metric_name} of {result_opt_model} saved to: {training_args.output_dir}."
+        f" Original model had an {metric_name} of {result_baseline_model}."
     )
 
     if optim_args.quantize and optim_args.verify_loading:
