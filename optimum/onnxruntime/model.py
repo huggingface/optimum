66c2da4c0220549bb6772bf787932d43459ea03c
Ella Charlaix <ella@huggingface.co>
2022-02-04 12:20:24 +0100

    Add default label names for evaluation of question answering tasks

--- optimum/onnxruntime/model.py
+++ optimum/onnxruntime/model.py
@@ -15,7 +15,7 @@
 import logging
 import os
 from pathlib import Path
-from typing import Callable, Dict, Optional, Union
+from typing import Callable, Dict, List, Optional, Union
 
 from torch.utils.data import DataLoader
 from transformers import EvalPrediction
@@ -36,6 +36,7 @@ class ORTModel:
         model_path: Union[str, os.PathLike],
         onnx_config: OnnxConfig,
         compute_metrics: Optional[Callable[[EvalPrediction], Dict]] = None,
+        label_names: Optional[List[str]] = None,
     ):
         """
         Args:
@@ -47,6 +48,8 @@ class ORTModel:
             compute_metrics (`Callable[[EvalPrediction], Dict]`, `optional`):
                 The function that will be used to compute metrics at evaluation. Must take an `EvalPrediction` and
                 return a dictionary string to metric values.
+            label_names (`List[str]`, `optional`):
+                The list of keys in your dictionary of inputs that correspond to the labels.
         """
         if not isinstance(onnx_config, OnnxConfig):
             raise TypeError(
@@ -59,6 +62,10 @@ class ORTModel:
         self.onnx_config = onnx_config
         self.model_path = Path(model_path)
         self.compute_metrics = compute_metrics
+        default_label_names = (
+            ["start_positions", "end_positions"] if self.onnx_config.task == "question-answering" else ["labels"]
+        )
+        self.label_names = default_label_names if label_names is None else label_names
 
     def evaluation_loop(self, dataloader: DataLoader):
         """
@@ -76,8 +83,13 @@ class ORTModel:
         session = InferenceSession(self.model_path.as_posix(), options)
         for step, inputs in enumerate(dataloader):
             onnx_inputs = {}
-            labels = inputs.get("labels").numpy()
-            num_samples += len(labels)
+            has_labels = all(inputs.get(k) is not None for k in self.label_names)
+            if has_labels:
+                labels = nested_numpify(tuple(inputs.get(name) for name in self.label_names))
+                if len(labels) == 1:
+                    labels = labels[0]
+            else:
+                labels = None
             for name, value in inputs.items():
                 if name in self.onnx_named_inputs:
                     if isinstance(value, (list, tuple)):
@@ -87,6 +99,7 @@ class ORTModel:
                         )
                     else:
                         onnx_inputs[name] = value.numpy()
+            num_samples += onnx_inputs[name].shape[0]
             preds = session.run(self.onnx_named_outputs, onnx_inputs)
             all_preds = preds if all_preds is None else nested_concat(all_preds, preds, padding_index=-100)
             all_labels = labels if all_labels is None else nested_concat(all_labels, labels, padding_index=-100)
