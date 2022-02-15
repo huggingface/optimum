e2b9ff2d7b7ca20e30c4439b8c66780b0b555c1f
Ella Charlaix <80481427+echarlaix@users.noreply.github.com>
2022-02-15 17:44:00 +0100

    Fix broken intel neural compressor install path requirements (#78)

--- examples/inc/pytorch/text-classification/run_glue.py
+++ examples/inc/pytorch/text-classification/run_glue.py
@@ -43,6 +43,7 @@ from transformers import (
 from transformers.trainer_utils import get_last_checkpoint
 from transformers.utils import check_min_version
 from transformers.utils.fx import symbolic_trace
+from transformers.utils.versions import require_version
 
 import yaml
 from optimum.intel.neural_compressor import (
@@ -64,6 +65,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = ""
 # Will error if the minimal version of Transformers is not installed. Remove at your own risks.
 check_min_version("4.12.0")
 
+require_version("datasets>=1.8.0", "To fix: pip install -r examples/inc/pytorch/text-classification/requirements.txt")
 
 task_to_keys = {
     "cola": ("sentence", None),
