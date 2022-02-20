f2607e88f0d96dd781e026e9621ace2dc9af51be
JingyaHuang <huang_jingya@outlook.com>
2022-02-20 21:33:21 +0100

    Replace register list by using FeaturesManager

--- optimum/onnx/auto/configuration_onnx_auto.py
+++ optimum/onnx/auto/configuration_onnx_auto.py
@@ -15,6 +15,7 @@
 """ Auto ONNX Config class."""
 import importlib
 import re
+from tarfile import SUPPORTED_TYPES
 import warnings
 from collections import OrderedDict
 from typing import List, Union
@@ -30,28 +31,21 @@ from transformers.models.auto.configuration_auto import (
 )
 from transformers.models.auto.dynamic import get_class_from_dynamic_module
 from transformers.onnx.config import OnnxConfig
+from transformers.onnx.features import FeaturesManager
 from transformers.utils import logging
 
 
 logger = logging.get_logger(__name__)
 
+ONNX_SUPPORTED_MODELS = FeaturesManager._SUPPORTED_MODEL_TYPE.keys()
+
 ONNX_CONFIG_MAPPING_NAMES = OrderedDict(
-    [
-        # Add configs here
-        ("albert", "AlbertOnnxConfig"),
-        ("bart", "BartOnnxConfig"),
-        ("bert", "BertOnnxConfig"),
-        ("camembert", "CamembertOnnxConfig"),
-        ("distilbert", "DistilBertOnnxConfig"),
-        ("gpt2", "GPT2OnnxConfig"),
-        ("ibert", "IBertOnnxConfig"),
-        ("longformer", "LongformerOnnxConfig"),
-        ("mbart", "MBartOnnxConfig"),
-        ("roberta", "RobertaOnnxConfig"),
-        ("t5", "T5OnnxConfig"),
-        ("xlm-roberta", "XLMRobertaOnnxConfig"),
-        ("xlm-roberta-xl", "XLMRobertaXLOnnxConfig"),
-    ]
+    [(key, FeaturesManager._SUPPORTED_MODEL_TYPE[key]['default'].func.__self__.__name__) 
+    for key in ONNX_SUPPORTED_MODELS if key in CONFIG_MAPPING.keys()]
+)
+
+ONNX_MODEL_NAMES_MAPPING = OrderedDict(
+    [(key, MODEL_NAMES_MAPPING[key]) for key in ONNX_SUPPORTED_MODELS if key in CONFIG_MAPPING.keys()]
 )
 
 
@@ -78,10 +72,10 @@ def _list_model_options(indent, config_to_class=None, use_model_types=True):
             model_type_to_name = {
                 model_type: _get_class_name(model_class)
                 for model_type, model_class in config_to_class.items()
-                if model_type in MODEL_NAMES_MAPPING
+                if model_type in ONNX_MODEL_NAMES_MAPPING
             }
         lines = [
-            f"{indent}- **{model_type}** -- {model_type_to_name[model_type]} ({MODEL_NAMES_MAPPING[model_type]} model)"
+            f"{indent}- **{model_type}** -- {model_type_to_name[model_type]} ({ONNX_MODEL_NAMES_MAPPING[model_type]} model)"
             for model_type in sorted(model_type_to_name.keys())
         ]
     else:
@@ -91,7 +85,7 @@ def _list_model_options(indent, config_to_class=None, use_model_types=True):
             if config in ONNX_CONFIG_MAPPING_NAMES
         }
         config_to_model_name = {
-            config: MODEL_NAMES_MAPPING[model_type] for model_type, config in ONNX_CONFIG_MAPPING_NAMES.items()
+            config: ONNX_MODEL_NAMES_MAPPING[model_type] for model_type, config in ONNX_CONFIG_MAPPING_NAMES.items()
         }
         lines = [
             f"{indent}- [`{config_name}`] configuration class: {config_to_name[config_name]} ({config_to_model_name[config_name]} model)"
@@ -144,15 +138,82 @@ class AutoOnnxConfig(AutoConfig):
         )
 
     @classmethod
-    # @replace_list_option_in_docstrings()
+    @replace_list_option_in_docstrings()
     def from_pretrained(cls, pretrained_model_name_or_path, task="default", **kwargs):
-        # TODO: Add docstring
-
+        r"""
+        Instantiate one of the onnx configuration classes of the library from a pretrained model configuration.
+        The configuration class to instantiate is selected based on the `model_type` property of the config object that
+        is loaded, or when it's missing, by falling back to using pattern matching on `pretrained_model_name_or_path`:
+        List options
+        Args:
+            pretrained_model_name_or_path (`str` or `os.PathLike`):
+                Can be either:
+                    - A string, the *model id* of a pretrained model configuration hosted inside a model repo on
+                      huggingface.co. Valid model ids can be located at the root-level, like `bert-base-uncased`, or
+                      namespaced under a user or organization name, like `dbmdz/bert-base-german-cased`.
+                    - A path to a *directory* containing a configuration file saved using the
+                      [`~PretrainedConfig.save_pretrained`] method, or the [`~PreTrainedModel.save_pretrained`] method,
+                      e.g., `./my_model_directory/`.
+                    - A path or url to a saved configuration JSON *file*, e.g.,
+                      `./my_model_directory/configuration.json`.
+            cache_dir (`str` or `os.PathLike`, *optional*):
+                Path to a directory in which a downloaded pretrained model configuration should be cached if the
+                standard cache should not be used.
+            force_download (`bool`, *optional*, defaults to `False`):
+                Whether or not to force the (re-)download the model weights and configuration files and override the
+                cached versions if they exist.
+            resume_download (`bool`, *optional*, defaults to `False`):
+                Whether or not to delete incompletely received files. Will attempt to resume the download if such a
+                file exists.
+            proxies (`Dict[str, str]`, *optional*):
+                A dictionary of proxy servers to use by protocol or endpoint, e.g., `{'http': 'foo.bar:3128',
+                'http://hostname': 'foo.bar:4012'}`. The proxies are used on each request.
+            revision(`str`, *optional*, defaults to `"main"`):
+                The specific model version to use. It can be a branch name, a tag name, or a commit id, since we use a
+                git-based system for storing models and other artifacts on huggingface.co, so `revision` can be any
+                identifier allowed by git.
+            return_unused_kwargs (`bool`, *optional*, defaults to `False`):
+                If `False`, then this function returns just the final configuration object.
+                If `True`, then this functions returns a `Tuple(config, unused_kwargs)` where *unused_kwargs* is a
+                dictionary consisting of the key/value pairs whose keys are not configuration attributes: i.e., the
+                part of `kwargs` which has not been used to update `config` and is otherwise ignored.
+            trust_remote_code (`bool`, *optional*, defaults to `False`):
+                Whether or not to allow for custom models defined on the Hub in their own modeling files. This option
+                should only be set to `True` for repositories you trust and in which you have read the code, as it will
+                execute code present on the Hub on your local machine.
+            kwargs(additional keyword arguments, *optional*):
+                The values in kwargs of any keys which are configuration attributes will be used to override the loaded
+                values. Behavior concerning key/value pairs whose keys are *not* configuration attributes is controlled
+                by the `return_unused_kwargs` keyword parameter.
+        Examples:
+        ```python
+        >>> from optimum.onnx.auto.configuration_onnx_auto import AutoOnnxConfig
+        >>> # Download configuration from huggingface.co and cache.
+        >>> config = AutoOnnxConfig.from_pretrained("bert-base-uncased", task="default")
+        >>> # Download configuration from huggingface.co (user-uploaded) and cache.
+        >>> config = AutoOnnxConfig.from_pretrained("dbmdz/bert-base-german-cased", task="default")
+        >>> # If configuration file is in a directory (e.g., was saved using *save_pretrained('./test/saved_model/')*).
+        >>> config = AutoOnnxConfig.from_pretrained("./test/bert_saved_model/", task="default")
+        >>> # Load a specific configuration file.
+        >>> config = AutoOnnxConfig.from_pretrained("./test/bert_saved_model/my_configuration.json", task="default")
+        >>> # Change some config attributes when loading a pretrained config.
+        >>> config = AutoOnnxConfig.from_pretrained("bert-base-uncased", task="default", output_attentions=True, foo=False)
+        >>> config.output_attentions
+        True
+        >>> config, unused_kwargs = AutoOnnxConfig.from_pretrained(
+        ...     "bert-base-uncased", task="default", output_attentions=True, foo=False, return_unused_kwargs=True
+        ... )
+        >>> config.output_attentions
+        True
+        >>> config.unused_kwargs
+        {'foo': False}
+        ```"""
         kwargs["_from_auto"] = True
         kwargs["name_or_path"] = pretrained_model_name_or_path
         trust_remote_code = kwargs.pop("trust_remote_code", False)
         config_dict, _ = PretrainedConfig.get_config_dict(pretrained_model_name_or_path, **kwargs)
-        if "auto_map" in config_dict and "AutoOnnxConfig" in config_dict["auto_map"]:
+        if "auto_map" in config_dict and "AutoConfig" in config_dict["auto_map"]:
+            # When given a custom configuaration
             if not trust_remote_code:
                 raise ValueError(
                     f"Loading {pretrained_model_name_or_path} requires you to execute the configuration file in that repo "
@@ -165,20 +226,28 @@ class AutoOnnxConfig(AutoConfig):
                     "ensure no malicious code has been contributed in a newer revision."
                 )
             class_ref = config_dict["auto_map"]["AutoConfig"]
-            onnx_class_ref = config_dict["auto_map"]["AutoOnnxConfig"]  # TODO: confirm the standard config file
             module_file, class_name = class_ref.split(".")
-            onnx_module_file, onnx_class_name = onnx_class_ref.split(".")
+
+            if config_dict["auto_map"]["AutoOnnxConfig"]: # TODO: to confirm the term name
+                onnx_class_ref = config_dict["auto_map"]["AutoOnnxConfig"]  
+                onnx_module_file, onnx_class_name = onnx_class_ref.split(".")
+            else:
+                onnx_module_file = module_file
+                onnx_class_name = re.sub(r'(.*)(Config)', r'\1Onnx\2', class_name)
+            
             config_class = get_class_from_dynamic_module(
                 pretrained_model_name_or_path, module_file + ".py", class_name, **kwargs
             )
             onnx_config_class = get_class_from_dynamic_module(
                 pretrained_model_name_or_path, onnx_module_file + ".py", onnx_class_name, **kwargs
             )
+            #TODO: action when `task` is not supported by the model
             return onnx_config_class(config_class.from_pretrained(pretrained_model_name_or_path, **kwargs), task=task)
         elif "model_type" in config_dict:
-            config_class = CONFIG_MAPPING[config_dict["model_type"]]
-            onnx_config_class = ONNX_CONFIG_MAPPING[config_dict["model_type"]]
-            return onnx_config_class(config_class.from_dict(config_dict, **kwargs), task=task)
+            #TODO: action when `model_type` doesn't have defined OnnxConfig
+            config_class = AutoConfig.from_pretrained(pretrained_model_name_or_path)
+            onnx_config_class = FeaturesManager._SUPPORTED_MODEL_TYPE[config_dict["model_type"]][task]
+            return onnx_config_class(config_class)
         else:
             # Fallback: use pattern matching on the string.
             for pattern in ONNX_CONFIG_MAPPING.keys():
--- tests/onnx/test_configuration_auto.py
+++ tests/onnx/test_configuration_auto.py
@@ -1,20 +1,5 @@
 # coding=utf-8
-# Copyright 2019-present, the HuggingFace Inc. team.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# coding=utf-8
-# Copyright 2019-present, the HuggingFace Inc. team.
+# Copyright 2022-present, the HuggingFace Inc. team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -32,27 +17,74 @@ import importlib
 import os
 import tempfile
 import unittest
+from parameterized import parameterized
 
+from transformers import is_tf_available, is_torch_available
+if is_torch_available() or is_tf_available():
+    from transformers.onnx.features import FeaturesManager
 from transformers.onnx import OnnxConfig
+from transformers.testing_utils import require_torch, slow
 
 from optimum.onnx.auto.configuration_onnx_auto import AutoOnnxConfig
 
+PYTORCH_EXPORT_MODELS = {
+    ("albert", "hf-internal-testing/tiny-albert"),
+    ("bert", "bert-base-cased"),
+    ("ibert", "kssteven/ibert-roberta-base"),
+    ("camembert", "camembert-base"),
+    ("distilbert", "distilbert-base-cased"),
+    ("roberta", "roberta-base"),
+    ("xlm-roberta", "xlm-roberta-base"),
+    ("layoutlm", "microsoft/layoutlm-base-uncased"),
+}
 
-SAMPLE_ROBERTA_CONFIG = os.path.join(os.path.dirname(os.path.abspath(__file__)), "fixtures/dummy-config.json")
+PYTORCH_EXPORT_WITH_PAST_MODELS = {
+    ("gpt2", "gpt2"),
+    ("gpt-neo", "EleutherAI/gpt-neo-125M"), 
+}
+
+PYTORCH_EXPORT_SEQ2SEQ_WITH_PAST_MODELS = {
+    ("bart", "facebook/bart-base"),
+    ("mbart", "sshleifer/tiny-mbart"),
+    ("t5", "t5-small"),
+    ("marian", "Helsinki-NLP/opus-mt-en-de"),
+}
+
+PYTORCH_MODELS_UNSUPPORTED = {
+    ("electra", "google/electra-base-generator"),
+    ("deberta", "microsoft/deberta-base"),
+    ("hubert", "facebook/hubert-base-ls960")
+}
+
+def _get_models_to_test(export_models_list):
+    models_to_test = []
+    if is_torch_available() or is_tf_available():
+        for (name, model) in export_models_list:
+            for feature, onnx_config_class_constructor in FeaturesManager.get_supported_features_for_model_type(
+                name
+            ).items():
+                models_to_test.append((f"{name}_{feature}", name, model, feature, onnx_config_class_constructor))
+        return sorted(models_to_test)
+    else:
+        # Returning some dummy test that should not be ever called because of the @require_torch / @require_tf
+        # decorators.
+        # The reason for not returning an empty list is because parameterized.expand complains when it's empty.
+        return [("dummy", "dummy", "dummy", "dummy", OnnxConfig.from_model_config)]
 
 
 class AutoOnnxConfigTest(unittest.TestCase):
-    def test_config_from_model_shortcut(self):
-        model_names = {"bert-base-cased", "distilbert-base-uncased", "facebook/bart-base", "gpt2", "roberta-base"}
-        task_names = {"default", "masked-lm", "sequence-classification"}
 
-        for model_name in model_names:
-            for task_name in task_names:
-                with self.subTest(model_name=model_name):
-                    config = AutoOnnxConfig.from_pretrained(model_name, task=task_name)
-                    self.assertIsInstance(config, OnnxConfig)
-                    print(type(config))
-                    print(config.outputs)
+    @parameterized.expand(_get_models_to_test(PYTORCH_EXPORT_MODELS))
+    @require_torch
+    def test_config_from_supported_model(self, test_name, name, model_name, feature, onnx_config_class_constructor):
+        config = AutoOnnxConfig.from_pretrained(model_name, task=feature)
+        self.assertIsInstance(config, OnnxConfig)
+    
+    @parameterized.expand(_get_models_to_test(PYTORCH_MODELS_UNSUPPORTED))
+    @require_torch
+    def test_config_from_unsupported_model(self, test_name, name, model_name, feature, onnx_config_class_constructor):
+        config = AutoOnnxConfig.from_pretrained(model_name, task=feature)
+        self.assertIsInstance(config, OnnxConfig)
 
 
 if __name__ == "__main__":
