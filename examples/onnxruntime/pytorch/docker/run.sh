8c9f93c7a441bfebbbc63862def67ea5e7a0490c
JingyaHuang <huang_jingya@outlook.com>
2022-02-21 22:57:41 +0100

    Fix TTY

--- examples/onnxruntime/pytorch/docker/run.sh
+++ examples/onnxruntime/pytorch/docker/run.sh
@@ -3,6 +3,10 @@
 CMD=${1:-/bin/bash}
 GPU_DEVICES=${2:-"all"}
 
+# Install nvidia docker toolkits
+sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+sudo systemctl restart docker 
+
 # Build Image
 docker build -f Dockerfile-cu10  -t ort/cu10 .
 # docker build -f Dockerfile-cu11  -t ort/cu11 .
