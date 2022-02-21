f251c9c9a300a1560b7512b07474ee08871e8482
JingyaHuang <huang_jingya@outlook.com>
2022-02-21 19:07:08 +0100

    add dockerfiles and bash script to test the workflow

---
+++ examples/onnxruntime/pytorch/docker/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CMD=${1:-/bin/bash}
+GPU_DEVICES=${2:-"all"}
+
+# Build Image
+docker build -f Dockerfile-cu10  -t ort/cu10 .
+# docker build -f Dockerfile-cu11  -t ort/cu11 .
+
+# Run Image
+docker run -it --rm -p 80:8888 --gpus $GPU_DEVICES ort/cu10:latest $CMD
+# docker run -it --rm -p 80:8888 --gpus $GPU_DEVICES ort/cu11:latest $CMD
