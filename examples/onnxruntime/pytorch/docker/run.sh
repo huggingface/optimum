4861542386d6e9fabd2574eed8e4121ae520faa1
JingyaHuang <huang_jingya@outlook.com>
2022-02-22 00:18:55 +0100

    Fix GPU device no found

--- examples/onnxruntime/pytorch/docker/run.sh
+++ examples/onnxruntime/pytorch/docker/run.sh
@@ -4,7 +4,7 @@ CMD=${1:-/bin/bash}
 GPU_DEVICES=${2:-"all"} 
 
 # Build Image
-# docker build -f Dockerfile-cu10  -t ort/cu10 .
+docker build -f Dockerfile-cu10  -t ort/cu10 .
 # docker build -f Dockerfile-cu11  -t ort/cu11 .
 
 # Install nvidia docker toolkits
@@ -13,9 +13,8 @@ GPU_DEVICES=${2:-"all"}
 # systemctl restart docker
 curl https://nvidia.github.io/nvidia-docker/centos7/nvidia-docker.repo > /etc/yum.repos.d/nvidia-docker.repo
 sudo yum update -y && yum install -y nvidia-container-toolkit
-# apt-get update && sudo apt-get install -y nvidia-container-toolkit
 sudo systemctl restart docker
 
 # Run Image
-# docker run --rm -p 80:8888 --gpus $GPU_DEVICES ort/cu10:latest $CMD
+docker run --rm -p 80:8888 --gpus $GPU_DEVICES ort/cu10:latest $CMD
 # docker run -it --rm -p 80:8888 --gpus $GPU_DEVICES ort/cu11:latest $CMD
