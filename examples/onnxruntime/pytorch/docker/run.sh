4297ab72d0403dbe39c52de684fe1bb97fa98ea9
JingyaHuang <huang_jingya@outlook.com>
2022-02-22 12:37:37 +0100

    Fix GPU device no found

--- examples/onnxruntime/pytorch/docker/run.sh
+++ examples/onnxruntime/pytorch/docker/run.sh
@@ -8,19 +8,21 @@ GPU_DEVICES=${2:-"all"}
 # docker build -f Dockerfile-cu11  -t ort/cu11 .
 
 # Re-install docker
-sudo subscription-manager repos --enable rhel-7-server-extras-rpms
-sudo yum install docker -y
-sudo systemctl --now enable docker
-sudo docker -v
-sudo docker run --rm hello-world
+# sudo yum install subscription-manager
+# sudo subscription-manager repos --enable rhel-7-server-extras-rpms
+# sudo yum install docker -y
+# sudo systemctl --now enable docker
+# sudo docker -v
+# sudo docker run --rm hello-world
 # Install nvidia docker toolkits
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
    && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.repo | sudo tee /etc/yum.repos.d/nvidia-docker.repo
-sudo yum clean expire-cache
-sudo yum install nvidia-container-toolkit -y
-sudo systemctl daemon-reload
-sudo systemctl restart docker
-sudo docker run --rm -e NVIDIA_VISIBLE_DEVICES=all nvidia/cuda:11.0-base nvidia-smi
+cat /etc/yum.repos.d/nvidia-docker.repo
+# sudo yum clean expire-cache
+# sudo yum install nvidia-container-toolkit -y
+# sudo systemctl daemon-reload
+# sudo systemctl restart docker
+# sudo docker run --rm -e NVIDIA_VISIBLE_DEVICES=all nvidia/cuda:11.0-base nvidia-smi
 # sudo yum update -y && yum install -y nvidia-container-toolkit
 # sudo systemctl daemon-reload
 # sudo systemctl restart docker
