bb2f2f519a2ba4213a78dc35ed8496e4c0561619
JingyaHuang <huang_jingya@outlook.com>
2022-02-22 14:22:42 +0100

    Fix GPU device no found

--- examples/onnxruntime/pytorch/docker/run.sh
+++ examples/onnxruntime/pytorch/docker/run.sh
@@ -4,7 +4,7 @@ CMD=${1:-/bin/bash}
 GPU_DEVICES=${2:-"all"} 
 
 # Build Image
-# docker build -f Dockerfile-cu10  -t ort/cu10 .
+docker build -f Dockerfile-cu10  -t ort/cu10 .
 # docker build -f Dockerfile-cu11  -t ort/cu11 .
 
 # Re-install docker
@@ -15,9 +15,9 @@ GPU_DEVICES=${2:-"all"}
 # sudo docker -v
 # sudo docker run --rm hello-world
 # Install nvidia docker toolkits
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.repo | sudo tee /etc/yum.repos.d/nvidia-docker.repo
-cat /etc/yum.repos.d/nvidia-docker.repo
+# distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+#    && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.repo | sudo tee /etc/yum.repos.d/nvidia-docker.repo
+# cat /etc/yum.repos.d/nvidia-docker.repo
 # sudo yum clean expire-cache
 # sudo yum install nvidia-container-toolkit -y
 # sudo systemctl daemon-reload
@@ -28,5 +28,5 @@ cat /etc/yum.repos.d/nvidia-docker.repo
 # sudo systemctl restart docker
 
 # Run Image
-# docker run --rm -p 80:8888 --gpus $GPU_DEVICES ort/cu10:latest $CMD
+docker run --rm -p 80:8888 --gpus $GPU_DEVICES ort/cu10:latest $CMD
 # docker run -it --rm -p 80:8888 --gpus $GPU_DEVICES ort/cu11:latest $CMD
