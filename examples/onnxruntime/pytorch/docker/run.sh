eff6baa573c65267b164075dd45d5f530d42ea9e
JingyaHuang <huang_jingya@outlook.com>
2022-02-27 22:11:14 +0000

    Install docker-compose

--- examples/onnxruntime/pytorch/docker/run.sh
+++ examples/onnxruntime/pytorch/docker/run.sh
@@ -1,31 +1,19 @@
 #!/bin/bash
 
 CMD=${1:-/bin/bash}
-GPU_DEVICES=${2:-"all"} 
+GPU_DEVICES=${2:-"all"}
+
+# Install docker-compose
+# 下载最新版本的 docker-compose 到 /usr/bin 目录下
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Check docker-compose
+docker-compose --version
 
 # Build Image
 docker build -f Dockerfile-cu10  -t ort/cu10 .
 # docker build -f Dockerfile-cu11  -t ort/cu11 .
-
-# Re-install docker
-# sudo yum install subscription-manager
-# sudo subscription-manager repos --enable rhel-7-server-extras-rpms
-# sudo yum install docker -y
-# sudo systemctl --now enable docker
-# sudo docker -v
-# sudo docker run --rm hello-world
-# Install nvidia docker toolkits
-# distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-#    && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.repo | sudo tee /etc/yum.repos.d/nvidia-docker.repo
-# cat /etc/yum.repos.d/nvidia-docker.repo
-# sudo yum clean expire-cache
-# sudo yum install nvidia-container-toolkit -y
-# sudo systemctl daemon-reload
-# sudo systemctl restart docker
-# sudo docker run --rm -e NVIDIA_VISIBLE_DEVICES=all nvidia/cuda:11.0-base nvidia-smi
-# sudo yum update -y && yum install -y nvidia-container-toolkit
-# sudo systemctl daemon-reload
-# sudo systemctl restart docker
 
 # Run Image
 docker run --rm -p 80:8888 --gpus $GPU_DEVICES ort/cu10:latest $CMD
