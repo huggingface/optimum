eb37091026e7529dbe3cb8f9f6e22eeb41003596
JingyaHuang <huang_jingya@outlook.com>
2022-02-27 22:52:53 +0000

    Install docker-compose

--- examples/onnxruntime/pytorch/docker/run.sh
+++ examples/onnxruntime/pytorch/docker/run.sh
@@ -3,15 +3,6 @@
 CMD=${1:-/bin/bash}
 GPU_DEVICES=${2:-"all"}
 
-# Install docker-compose
-# 下载最新版本的 docker-compose 到 /usr/bin 目录下
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-which docker-compose
-
-# Check docker-compose
-docker-compose --version
-
 # Build Image
 docker build -f Dockerfile-cu10  -t ort/cu10 .
 # docker build -f Dockerfile-cu11  -t ort/cu11 .
