a021db2317a38c5f36be4e7dce683e0029d0577c
JingyaHuang <huang_jingya@outlook.com>
2022-02-28 13:08:42 +0000

    Go with docker run

--- examples/onnxruntime/pytorch/docker/run.sh
+++ examples/onnxruntime/pytorch/docker/run.sh
@@ -4,9 +4,15 @@ CMD=${1:-/bin/bash}
 GPU_DEVICES=${2:-"all"}
 
 # Build Image
-docker build -f Dockerfile-cu10  -t ort/cu10 .
+# docker build -f Dockerfile-cu10  -t ort/cu10 .
 # docker build -f Dockerfile-cu11  -t ort/cu11 .
 
 # Run Image
-docker run --rm -p 80:8888 --gpus $GPU_DEVICES ort/cu10:latest $CMD
+# docker run --rm -p 80:8888 --gpus $GPU_DEVICES ort/cu10:latest $CMD
 # docker run -it --rm -p 80:8888 --gpus $GPU_DEVICES ort/cu11:latest $CMD
+
+# Install dependencies
+git clone https://github.com/huggingface/optimum.git
+cd optimum
+pip install .[onnxruntime]
+python -m unittest discover -s onnxruntime -p 'test_onnxruntime_train.py'  
