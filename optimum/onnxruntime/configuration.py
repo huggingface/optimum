6d3d966d9aa67e2a67b115a85502b10e161def01
Morgan Funtowicz <funtowiczmo@gmail.com>
2022-03-03 15:28:16 +0100

    Initial refactoring of the Quantization API.

--- optimum/onnxruntime/configuration.py
+++ optimum/onnxruntime/configuration.py
@@ -17,17 +17,14 @@ from enum import Enum
 from pathlib import Path
 from typing import Dict, List, Optional, Tuple, Union
 
-from datasets import Dataset
-from packaging.version import Version, parse
-
-from onnxruntime import GraphOptimizationLevel
-from onnxruntime import __version__ as ort_version
-from onnxruntime.quantization import CalibraterBase, CalibrationMethod, QuantFormat, QuantizationMode, QuantType
 from onnxruntime.quantization.calibrate import create_calibrator
-from optimum.onnxruntime import ORT_DEFAULT_CHANNEL_FOR_OPERATORS, ORTQuantizableOperator
+from datasets import Dataset
+from onnxruntime import GraphOptimizationLevel
+from onnxruntime.quantization import QuantFormat, QuantizationMode, QuantType, CalibrationMethod, CalibraterBase
+
+from optimum.onnxruntime import ORT_FULLY_CONNECTED_OPERATORS, ORT_DEFAULT_CHANNEL_FOR_OPERATORS
 
 from ..configuration_utils import BaseConfig
-
 
 NodeName = NodeType = str
 
@@ -42,84 +39,53 @@ class CalibrationConfig:
     num_bins: Optional[int] = None
     num_quantized_bins: Optional[int] = None
     percentiles: Optional[float] = None
-    moving_average: Optional[bool] = None
-    averaging_constant: Optional[float] = None
 
     def create_calibrator(
         self,
         onnx_model_path: Union[str, os.PathLike, Path],
-        operators_to_quantize: Optional[List[Union[NodeType, ORTQuantizableOperator]]] = [
-            ORTQuantizableOperator.FullyConnected
-        ],
+        operators_to_quantize: Optional[List[NodeType]],
         use_external_data_format: bool = False,
         force_symmetric_range: bool = False,
-        augmented_model_name: str = "augmented_model.onnx",
+        augmented_model_name: str = "augmented_model.onnx"
     ) -> CalibraterBase:
-
-        operators_to_calibrate = (operators_to_quantize or []).copy()
-
-        if ORTQuantizableOperator.FullyConnected in operators_to_calibrate:
-            operators_to_calibrate.remove(ORTQuantizableOperator.FullyConnected)
-            operators_to_calibrate += [ORTQuantizableOperator.MatMul, ORTQuantizableOperator.Add]
-
-        operators_to_calibrate = [
-            operator.value if isinstance(operator, ORTQuantizableOperator) else operator
-            for operator in operators_to_calibrate
-        ]
-
-        kwargs = {
-            "model": onnx_model_path,
-            "op_types_to_calibrate": operators_to_calibrate,
-            "calibrate_method": self.method,
-            "augmented_model_path": augmented_model_name,
-        }
-        if parse(ort_version) > Version("1.10.0"):
-            kwargs["use_external_data_format"] = use_external_data_format
-            kwargs["extra_options"] = {
-                "symmetric": force_symmetric_range,
+        return create_calibrator(
+            model=onnx_model_path,
+            op_types_to_calibrate=operators_to_quantize or [],
+            calibrate_method=self.method,
+            use_external_data_format=use_external_data_format,
+            augmented_model_path=augmented_model_name,
+            extra_options={
                 "num_bins": self.num_bins,
                 "num_quantized_bins": self.num_quantized_bins,
                 "percentiles": self.percentiles,
-                "moving_average": self.moving_average,
-                "averaging_constant": self.averaging_constant,
+                "symmetric": force_symmetric_range
             }
-        return create_calibrator(**kwargs)
+        )
 
 
 class AutoCalibrationConfig:
     @staticmethod
-    def minmax(dataset: Dataset, moving_average: bool = False, averaging_constant: float = 0.01) -> CalibrationConfig:
+    def minmax(
+        dataset: Dataset,
+    ) -> CalibrationConfig:
         """
 
         :param dataset: The dataset to use to calibrate the model
-        :param moving_average:
-        :param averaging_constant:
         :return:
         """
-        if moving_average and parse(ort_version) < Version("1.10.99"):
-            raise NotImplementedError(
-                "MinMax calibration method using the moving average for the activations quantization parameters "
-                "computation is only implemented for onnxruntime >= 1.11.0."
-            )
-
-        if moving_average and not 0 <= averaging_constant <= 1:
-            raise ValueError(f"Invalid averaging constant value ({averaging_constant}) should be within [0, 1]")
-
         return CalibrationConfig(
             dataset_name=dataset.info.builder_name,
             dataset_config_name=dataset.info.config_name,
             dataset_split=str(dataset.split),
             dataset_num_samples=dataset.num_rows,
-            method=CalibrationMethod.MinMax,
-            moving_average=moving_average,
-            averaging_constant=averaging_constant,
+            method=CalibrationMethod.MinMax
         )
 
     @staticmethod
     def entropy(
         dataset: Dataset,
         num_bins: int = 128,
-        num_quantized_bins: int = 128,
+        num_quantized_bins: int = 2048,
     ) -> CalibrationConfig:
         """
 
@@ -128,9 +94,6 @@ class AutoCalibrationConfig:
         :param num_quantized_bins:
         :return:
         """
-        if parse(ort_version) <= Version("1.10.99"):
-            raise NotImplementedError("entropy calibration method is only implemented for onnxruntime >= 1.11.0")
-
         if num_bins <= 0:
             raise ValueError(f"Invalid value num_bins ({num_bins}) should be >= 1")
 
@@ -142,7 +105,7 @@ class AutoCalibrationConfig:
             dataset_config_name=dataset.info.config_name,
             dataset_split=str(dataset.split),
             dataset_num_samples=dataset.num_rows,
-            method=CalibrationMethod.Entropy,
+            method=CalibrationMethod.MinMax,
             num_bins=num_bins,
             num_quantized_bins=num_quantized_bins,
         )
@@ -150,9 +113,9 @@ class AutoCalibrationConfig:
     @staticmethod
     def percentiles(
         dataset: Dataset,
-        num_bins: int = 2048,
-        num_quantized_bins: int = 128,
-        percentiles: float = 99.999,
+        num_bins: int = 128,
+        num_quantized_bins: int = 2048,
+        percentiles: float = 99.999
     ) -> CalibrationConfig:
         """
 
@@ -162,9 +125,6 @@ class AutoCalibrationConfig:
         :param percentiles:
         :return:
         """
-
-        # if parse(ort_version) <= Version("1.10.99"):
-        #     raise NotImplementedError("percentiles calibration method is only implemented for onnxruntime > 1.10.0")
 
         if num_bins <= 0:
             raise ValueError(f"Invalid value num_bins ({num_bins}) should be >= 1")
@@ -180,16 +140,15 @@ class AutoCalibrationConfig:
             dataset_config_name=dataset.info.config_name,
             dataset_split=str(dataset.split),
             dataset_num_samples=dataset.num_rows,
-            method=CalibrationMethod.Percentile,
+            method=CalibrationMethod.MinMax,
             num_bins=num_bins,
             num_quantized_bins=num_quantized_bins,
-            percentiles=percentiles,
+            percentiles=percentiles
         )
 
 
 @dataclass
 class QuantizationConfig:
-    is_static: bool
     format: QuantFormat.QDQ
     mode: QuantizationMode = QuantizationMode.QLinearOps
     activations_dtype: QuantType = QuantType.QInt8
@@ -199,33 +158,25 @@ class QuantizationConfig:
     per_channel: bool = False
     reduce_range: bool = False
     nodes_to_quantize: List[NodeName] = None
-    nodes_to_exclude: List[NodeName] = field(default_factory=list)
-    operators_to_quantize: List[NodeType] = field(default_factory=list)
+    nodes_to_exclude: List[NodeName] = None
+    operators_to_quantize: List[NodeType] = None
     qdq_add_pair_to_weight: bool = False
     qdq_dedicated_pair: bool = False
     qdq_op_type_per_channel_support_to_axis: Dict[str, int] = field(
         default_factory=lambda: ORT_DEFAULT_CHANNEL_FOR_OPERATORS
     )
 
-    def __post_init__(self):
-        ensure_valid_mode_or_raise(self.is_static, self.mode)
-        ensure_valid_data_type_or_raise(self.is_static, self.activations_dtype, self.weights_dtype)
-
     @staticmethod
     def quantization_type_str(activations_dtype: QuantType, weights_dtype: QuantType) -> str:
-        return (
-            f"{'s8' if activations_dtype == QuantType.QInt8 else 'u8'}"
-            f"/"
-            f"{'s8' if weights_dtype == QuantType.QInt8 else 'u8'}"
-        )
+        return f"{'s8' if activations_dtype == QuantType.QInt8 else 'u8'}" \
+               f"/" \
+               f"{'s8' if weights_dtype == QuantType.QInt8 else 'u8'}"
 
     def __str__(self):
-        return (
-            f"{self.format} ("
-            f"mode: {self.mode}, "
-            f"schema: {QuantizationConfig.quantization_type_str(self.activations_dtype, self.weights_dtype)}, "
-            f"channel-wise: {self.per_channel})"
-        )
+        return f"{self.format} (" \
+               f"mode: {self.mode}, " \
+               f"schema: {QuantizationConfig.quantization_type_str(self.activations_dtype, self.weights_dtype)}, " \
+               f"channel-wise: {self.per_channel})"
 
 
 def ensure_valid_mode_or_raise(use_static_quantization: bool, mode: QuantizationMode):
@@ -239,32 +190,10 @@ def ensure_valid_mode_or_raise(use_static_quantization: bool, mode: Quantization
         )
 
 
-def ensure_valid_data_type_or_raise(
-    use_static_quantization: bool, activations_dtype: QuantType, weights_dtype: QuantType
-):
-    if not use_static_quantization and activations_dtype == QuantType.QInt8:
-        raise ValueError(
-            "Invalid combination of "
-            "use_static_quantization = False "
-            "and "
-            "activations_dtype = QuantType.QInt8. "
-            "OnnxRuntime dynamic quantization requires activations_dtype = QuantType.QUInt8"
-        )
-
-    if use_static_quantization and activations_dtype == QuantType.QInt8 and weights_dtype == QuantType.QUInt8:
-        raise ValueError(
-            "Invalid combination of "
-            "use_static_quantization = True, "
-            "activations_dtype = QuantType.QInt8 "
-            "and "
-            "weights_dtype = QuantType.QUInt8."
-            "OnnxRuntime static quantization does not support "
-            "activations_dtype = QuantType.QInt8 with weights_dtype = QuantType.QUInt8."
-        )
-
-
 def default_quantization_parameters(
-    is_static: bool, format: Optional[QuantFormat] = None, mode: Optional[QuantizationMode] = None
+    is_static: bool,
+    format: Optional[QuantFormat] = None,
+    mode: Optional[QuantizationMode] = None
 ) -> Tuple[QuantFormat, QuantizationMode]:
     if format is None:
         format = QuantFormat.QDQ if is_static else QuantFormat.QOperator
@@ -276,6 +205,7 @@ def default_quantization_parameters(
 
 
 class AutoQuantizationConfig:
+
     @staticmethod
     def arm64(
         is_static: bool,
@@ -286,8 +216,8 @@ class AutoQuantizationConfig:
         per_channel: bool = True,
         nodes_to_quantize: Optional[List[NodeName]] = None,
         nodes_to_exclude: Optional[List[NodeName]] = None,
-        operators_to_quantize: List[Union[NodeType, ORTQuantizableOperator]] = [ORTQuantizableOperator.FullyConnected],
-    ) -> QuantizationConfig:
+        operators_to_quantize: List[NodeName] = ORT_FULLY_CONNECTED_OPERATORS
+    ):
         """
 
         :param is_static: Boolean flag to indicate whether we target static or dynamic quantization.
@@ -306,6 +236,8 @@ class AutoQuantizationConfig:
         :param operators_to_quantize:
         :return:
         """
+        ensure_valid_mode_or_raise(is_static, mode)
+
         if format is None:
             format = QuantFormat.QDQ if is_static else QuantFormat.QOperator
 
@@ -315,7 +247,6 @@ class AutoQuantizationConfig:
         # u8/s8 is faster (than u8/u8) on lower-end ARM64 and identical on higher-end ARM64,
         # so let's use u8/s8 by default
         return QuantizationConfig(
-            is_static=is_static,
             format=format,
             mode=mode,
             activations_dtype=QuantType.QUInt8,
@@ -324,9 +255,9 @@ class AutoQuantizationConfig:
             weights_symmetric=use_symmetric_weights,
             per_channel=per_channel,
             reduce_range=False,
-            nodes_to_quantize=nodes_to_quantize or [],
-            nodes_to_exclude=nodes_to_exclude or [],
-            operators_to_quantize=operators_to_quantize,
+            nodes_to_quantize=nodes_to_quantize,
+            nodes_to_exclude=nodes_to_exclude,
+            operators_to_quantize=operators_to_quantize
         )
 
     @staticmethod
@@ -340,7 +271,7 @@ class AutoQuantizationConfig:
         reduce_range: bool = False,
         nodes_to_quantize: Optional[List[NodeName]] = None,
         nodes_to_exclude: Optional[List[NodeName]] = None,
-        operators_to_quantize: List[Union[NodeType, ORTQuantizableOperator]] = [ORTQuantizableOperator.FullyConnected],
+        operators_to_quantize: List[NodeName] = ORT_FULLY_CONNECTED_OPERATORS
     ) -> QuantizationConfig:
         """
 
@@ -365,10 +296,10 @@ class AutoQuantizationConfig:
         :param operators_to_quantize:
         :return:
         """
+        ensure_valid_mode_or_raise(is_static, mode)
         format, mode = default_quantization_parameters(is_static, format, mode)
 
         return QuantizationConfig(
-            is_static=is_static,
             format=format,
             mode=mode,
             activations_dtype=QuantType.QUInt8,
@@ -377,9 +308,9 @@ class AutoQuantizationConfig:
             weights_symmetric=use_symmetric_weights,
             per_channel=per_channel,
             reduce_range=reduce_range,
-            nodes_to_quantize=nodes_to_quantize or [],
-            nodes_to_exclude=nodes_to_exclude or [],
-            operators_to_quantize=operators_to_quantize,
+            nodes_to_quantize=nodes_to_quantize,
+            nodes_to_exclude=nodes_to_exclude,
+            operators_to_quantize=operators_to_quantize
         )
 
     @staticmethod
@@ -393,7 +324,7 @@ class AutoQuantizationConfig:
         reduce_range: bool = False,
         nodes_to_quantize: Optional[List[NodeName]] = None,
         nodes_to_exclude: Optional[List[NodeName]] = None,
-        operators_to_quantize: List[Union[NodeType, ORTQuantizableOperator]] = [ORTQuantizableOperator.FullyConnected],
+        operators_to_quantize: List[NodeName] = ORT_FULLY_CONNECTED_OPERATORS,
     ) -> QuantizationConfig:
         """
 
@@ -418,10 +349,10 @@ class AutoQuantizationConfig:
         :param operators_to_quantize:
         :return:
         """
+        ensure_valid_mode_or_raise(is_static, mode)
         format, mode = default_quantization_parameters(is_static, format, mode)
 
         return QuantizationConfig(
-            is_static=is_static,
             format=format,
             mode=mode,
             activations_dtype=QuantType.QUInt8,
@@ -430,9 +361,9 @@ class AutoQuantizationConfig:
             weights_symmetric=use_symmetric_weights,
             per_channel=per_channel,
             reduce_range=reduce_range,
-            nodes_to_quantize=nodes_to_quantize or [],
-            nodes_to_exclude=nodes_to_exclude or [],
-            operators_to_quantize=operators_to_quantize,
+            nodes_to_quantize=nodes_to_quantize,
+            nodes_to_exclude=nodes_to_exclude,
+            operators_to_quantize=operators_to_quantize
         )
 
     @staticmethod
@@ -445,7 +376,7 @@ class AutoQuantizationConfig:
         per_channel: bool = True,
         nodes_to_quantize: Optional[List[NodeName]] = None,
         nodes_to_exclude: Optional[List[NodeName]] = None,
-        operators_to_quantize: List[Union[NodeType, ORTQuantizableOperator]] = [ORTQuantizableOperator.FullyConnected],
+        operators_to_quantize: List[NodeName] = ORT_FULLY_CONNECTED_OPERATORS
     ) -> QuantizationConfig:
         """
         When targeting Intel AVX512-VNNI CPU underlying execution engine leverage the CPU instruction VPDPBUSD to
@@ -472,10 +403,10 @@ class AutoQuantizationConfig:
         :param operators_to_quantize:
         :return:
         """
+        ensure_valid_mode_or_raise(is_static, mode)
         format, mode = default_quantization_parameters(is_static, format, mode)
 
         return QuantizationConfig(
-            is_static=is_static,
             format=format,
             mode=mode,
             activations_dtype=QuantType.QUInt8,
@@ -484,9 +415,9 @@ class AutoQuantizationConfig:
             weights_symmetric=use_symmetric_weights,
             per_channel=per_channel,
             reduce_range=False,
-            nodes_to_quantize=nodes_to_quantize or [],
-            nodes_to_exclude=nodes_to_exclude or [],
-            operators_to_quantize=operators_to_quantize,
+            nodes_to_quantize=nodes_to_quantize,
+            nodes_to_exclude=nodes_to_exclude,
+            operators_to_quantize=operators_to_quantize
         )
 
     @staticmethod
@@ -497,12 +428,12 @@ class AutoQuantizationConfig:
         per_channel: bool = True,
         nodes_to_quantize: Optional[List[NodeName]] = None,
         nodes_to_exclude: Optional[List[NodeName]] = None,
-        operators_to_quantize: List[Union[NodeType, ORTQuantizableOperator]] = [ORTQuantizableOperator.FullyConnected],
+        operators_to_quantize: List[NodeName] = ORT_FULLY_CONNECTED_OPERATORS,
     ) -> QuantizationConfig:
+        ensure_valid_mode_or_raise(is_static, mode)
         format, mode = default_quantization_parameters(is_static, format, mode)
 
         return QuantizationConfig(
-            is_static=is_static,
             format=format,
             mode=mode,
             activations_dtype=QuantType.QInt8,
@@ -511,11 +442,11 @@ class AutoQuantizationConfig:
             weights_symmetric=True,  # TRT only supports symmetric
             per_channel=per_channel,
             reduce_range=False,
-            nodes_to_quantize=nodes_to_quantize or [],
-            nodes_to_exclude=nodes_to_exclude or [],
+            nodes_to_quantize=nodes_to_quantize,
+            nodes_to_exclude=nodes_to_exclude,
             operators_to_quantize=operators_to_quantize,
             qdq_add_pair_to_weight=True,
-            qdq_dedicated_pair=True,
+            qdq_dedicated_pair=True
         )
 
 
@@ -531,7 +462,6 @@ class OptimizationConfig:
     (GraphOptimizationLevel.ORT_ENABLE_EXTENDED)
     99 will enable all available optimizations including layout optimizations. (GraphOptimizationLevel.ORT_ENABLE_ALL)
     """
-
     optimization_level: Union[int, GraphOptimizationLevel] = GraphOptimizationLevel.ORT_ENABLE_BASIC
 
     """
@@ -575,6 +505,7 @@ class ORTConfig(BaseConfig):
         super().__init__()
         self.opset = opset
         self.use_external_data_format = use_external_data_format
+
         self.optimization = self.dataclass_to_dict(optimization_config)
         self.quantization = self.dataclass_to_dict(quantization_config)
 
