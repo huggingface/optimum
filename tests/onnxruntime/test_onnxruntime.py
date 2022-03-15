d4e2de6a593410c4445c530dce7792e90e363345
Ella Charlaix <ella@huggingface.co>
2022-03-15 16:30:24 +0100

    Update onnxruntime test

--- tests/onnxruntime/test_onnxruntime.py
+++ tests/onnxruntime/test_onnxruntime.py
@@ -16,18 +16,22 @@ import gc
 import os
 import tempfile
 import unittest
+from functools import partial
 from pathlib import Path
 
 from transformers import AutoTokenizer
 from transformers.onnx import validate_model_outputs
 
-from optimum.onnxruntime import ORTConfig, ORTOptimizer, ORTQuantizer
+from optimum.onnxruntime import ORTModel, ORTOptimizer, ORTQuantizableOperator
+from optimum.onnxruntime.configuration import AutoCalibrationConfig, OptimizationConfig, ORTConfig, QuantizationConfig
+from optimum.onnxruntime.quantization import ORTQuantizer, QuantFormat, QuantizationMode, QuantType
 
 
 class TestORTOptimizer(unittest.TestCase):
     def test_optimize(self):
         model_names = {"bert-base-cased", "distilbert-base-uncased", "facebook/bart-base", "gpt2", "roberta-base"}
-        ort_config = ORTConfig(opt_level=1, only_onnxruntime=False)
+        optimization_config = OptimizationConfig(optimization_level=1, optimize_with_onnxruntime_only=False)
+        ort_config = ORTConfig(optimization_config=optimization_config)
         for model_name in model_names:
             with self.subTest(model_name=model_name):
                 with tempfile.TemporaryDirectory() as tmp_dir:
@@ -50,68 +54,94 @@ class TestORTOptimizer(unittest.TestCase):
 class TestORTQuantizer(unittest.TestCase):
     def test_dynamic_quantization(self):
         model_names = {"bert-base-cased", "distilbert-base-uncased", "facebook/bart-base", "gpt2", "roberta-base"}
-        ort_config = ORTConfig(
-            quantization_approach="dynamic",
+
+        qconfig = QuantizationConfig(
+            is_static=False,
+            format=QuantFormat.QOperator,
+            mode=QuantizationMode.IntegerOps,
+            activations_dtype=QuantType.QUInt8,
+            weights_dtype=QuantType.QInt8,
             per_channel=False,
             reduce_range=False,
-            weight_type="uint8",
-            activation_type="uint8",
+            operators_to_quantize=[ORTQuantizableOperator.FullyConnected],
         )
+
         for model_name in model_names:
             with self.subTest(model_name=model_name):
                 with tempfile.TemporaryDirectory() as tmp_dir:
                     output_dir = Path(tmp_dir)
+                    model_path = output_dir.joinpath("model.onnx")
                     q8_model_path = output_dir.joinpath("model-quantized.onnx")
-                    quantizer = ORTQuantizer(ort_config)
-                    quantizer.fit(model_name, output_dir, feature="sequence-classification")
+                    quantizer = ORTQuantizer.from_pretrained(model_name, feature="sequence-classification")
+                    quantizer.export(
+                        onnx_model_path=model_path,
+                        onnx_quantized_model_output_path=q8_model_path,
+                        calibration_tensors_range=None,
+                        quantization_config=qconfig,
+                    )
                     validate_model_outputs(
-                        quantizer.onnx_config,
+                        quantizer._onnx_config,
                         quantizer.tokenizer,
                         quantizer.model,
                         q8_model_path,
-                        list(quantizer.onnx_config.outputs.keys()),
+                        list(quantizer._onnx_config.outputs.keys()),
                         atol=8e-1,
                     )
                     gc.collect()
 
     def test_static_quantization(self):
-        model_names = {"distilbert-base-uncased"}
-        ort_config = ORTConfig(
-            quantization_approach="static",
+        model_names = {"bert-base-cased"}
+
+        qconfig = QuantizationConfig(
+            is_static=True,
+            format=QuantFormat.QDQ,
+            mode=QuantizationMode.QLinearOps,
+            activations_dtype=QuantType.QInt8,
+            weights_dtype=QuantType.QInt8,
             per_channel=False,
             reduce_range=False,
-            weight_type="uint8",
-            activation_type="uint8",
-            quant_format="operator",
-            calibration_method="minmax",
-            optimize_model=True,
-            split="train",
-            max_samples=80,
-            calib_batch_size=8,
+            operators_to_quantize=[ORTQuantizableOperator.FullyConnected],
         )
 
-        def preprocess_function(examples):
+        def preprocess_function(examples, tokenizer):
             return tokenizer(examples["sentence"], padding="max_length", max_length=128, truncation=True)
 
         for model_name in model_names:
             with self.subTest(model_name=model_name):
                 with tempfile.TemporaryDirectory() as tmp_dir:
                     output_dir = Path(tmp_dir)
+
+                    model_path = output_dir.joinpath("model.onnx")
                     q8_model_path = output_dir.joinpath("model-quantized.onnx")
-                    quantizer = ORTQuantizer(
-                        ort_config,
-                        dataset_name="glue",
+                    quantizer = ORTQuantizer.from_pretrained(model_name, feature="sequence-classification")
+
+                    calibration_dataset = quantizer.get_calibration_dataset(
+                        "glue",
                         dataset_config_name="sst2",
-                        preprocess_function=preprocess_function,
+                        preprocess_function=partial(preprocess_function, tokenizer=quantizer.tokenizer),
+                        num_samples=40,
+                        dataset_split="train",
                     )
-                    tokenizer = AutoTokenizer.from_pretrained(model_name)
-                    quantizer.fit(model_name, output_dir, feature="sequence-classification")
+                    calibration_config = AutoCalibrationConfig.minmax(calibration_dataset)
+                    ranges = quantizer.fit(
+                        dataset=calibration_dataset,
+                        calibration_config=calibration_config,
+                        onnx_model_path=model_path,
+                    )
+
+                    quantizer.export(
+                        onnx_model_path=model_path,
+                        onnx_quantized_model_output_path=q8_model_path,
+                        calibration_tensors_range=ranges,
+                        quantization_config=qconfig,
+                    )
+
                     validate_model_outputs(
-                        quantizer.onnx_config,
-                        tokenizer,
+                        quantizer._onnx_config,
+                        quantizer.tokenizer,
                         quantizer.model,
                         q8_model_path,
-                        list(quantizer.onnx_config.outputs.keys()),
+                        list(quantizer._onnx_config.outputs.keys()),
                         atol=5e-1,
                     )
                     gc.collect()
