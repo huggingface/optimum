aa2566fa3592fb91b4174a035d108677f09bc575
JingyaHuang <huang_jingya@outlook.com>
2022-03-28 07:12:51 +0000

    Add test for seq2seq orttrainer

--- tests/onnxruntime/test_onnxruntime_train.py
+++ tests/onnxruntime/test_onnxruntime_train.py
@@ -18,15 +18,15 @@ from enum import Enum
 from pathlib import Path
 
 import datasets
+import nltk
 import numpy as np
-import transformers
 from datasets import load_dataset, load_metric
 from transformers import (
-    AutoConfig,
-    AutoModel,
+    AutoModelForSeq2SeqLM,
     AutoModelForSequenceClassification,
     AutoTokenizer,
-    BartTokenizer,
+    DataCollatorForSeq2Seq,
+    Seq2SeqTrainingArguments,
     TrainingArguments,
     default_data_collator,
 )
@@ -34,7 +34,7 @@ from transformers.onnx import export, validate_model_outputs
 from transformers.onnx.features import FeaturesManager
 from transformers.trainer import Trainer
 
-from optimum.onnxruntime import ORTTrainer
+from optimum.onnxruntime import ORTTrainer, Seq2SeqORTTrainer
 
 
 class TestORTTrainer(unittest.TestCase):
@@ -120,6 +120,119 @@ class TestORTTrainer(unittest.TestCase):
                         print("Training metrics(ORT):\n", train_metrics)
                         print("Evaluation metrics(ORT):\n", ort_eval_metrics)
                         print("Prediction results(ORT):\n", ort_prediction)
+
+    def test_ort_seq2seq_trainer(self):
+
+        model_names = {"t5-small"}
+        dataset_names = {"xsum"}
+        metric_name = "rouge"
+        batch_size = 8
+        learning_rate = 2e-5
+        weight_decay = 0.01
+        num_train_epochs = 1
+
+        for model_name in model_names:
+            for dataset_name in dataset_names:
+                with self.subTest(model_name=model_name, dataset_name=dataset_name):
+                    with tempfile.TemporaryDirectory() as tmp_dir:
+
+                        # Prepare model
+                        model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+                        tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+                        # Prepare dataset
+                        dataset = load_dataset(dataset_name)
+                        metric = load_metric(metric_name)
+                        data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
+
+                        if model_name in ["t5-small", "t5-base", "t5-large", "t5-3b", "t5-11b"] and dataset_name in [
+                            "xsum"
+                        ]:
+                            prefix = "summarize: "
+                        else:
+                            prefix = ""
+
+                        max_input_length = 1024
+                        max_target_length = 128
+
+                        def preprocess_function(examples):
+                            inputs = [prefix + doc for doc in examples["document"]]
+                            model_inputs = tokenizer(inputs, max_length=max_input_length, truncation=True)
+
+                            # Setup the tokenizer for targets
+                            with tokenizer.as_target_tokenizer():
+                                labels = tokenizer(examples["summary"], max_length=max_target_length, truncation=True)
+
+                            model_inputs["labels"] = labels["input_ids"]
+                            return model_inputs
+
+                        encoded_dataset = dataset.map(preprocess_function, batched=True)
+                        max_train_samples = 200
+                        max_valid_samples = 50
+                        max_test_samples = 20
+                        train_dataset = encoded_dataset["train"]  # .select(range(max_train_samples))
+                        valid_dataset = encoded_dataset["validation"]  # .select(range(max_valid_samples))
+                        test_dataset = encoded_dataset["test"].remove_columns(
+                            ["label"]
+                        )  # .select(range(max_test_samples))
+
+                        def compute_metrics(eval_pred):
+                            predictions, labels = eval_pred
+                            decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+                            # Replace -100 in the labels as we can't decode them.
+                            labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+                            decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+                            # Rouge expects a newline after each sentence
+                            decoded_preds = ["\n".join(nltk.sent_tokenize(pred.strip())) for pred in decoded_preds]
+                            decoded_labels = ["\n".join(nltk.sent_tokenize(label.strip())) for label in decoded_labels]
+
+                            result = metric.compute(
+                                predictions=decoded_preds, references=decoded_labels, use_stemmer=True
+                            )
+                            # Extract a few results
+                            result = {key: value.mid.fmeasure * 100 for key, value in result.items()}
+
+                            # Add mean generated length
+                            prediction_lens = [
+                                np.count_nonzero(pred != tokenizer.pad_token_id) for pred in predictions
+                            ]
+                            result["gen_len"] = np.mean(prediction_lens)
+
+                            return {k: round(v, 4) for k, v in result.items()}
+
+                        training_args = Seq2SeqTrainingArguments(
+                            f"{model_name}-finetuned",
+                            evaluation_strategy="epoch",
+                            learning_rate=learning_rate,
+                            per_device_train_batch_size=batch_size,
+                            per_device_eval_batch_size=batch_size,
+                            weight_decay=weight_decay,
+                            save_total_limit=3,
+                            num_train_epochs=num_train_epochs,
+                            predict_with_generate=True,
+                            fp16=True,
+                        )
+
+                        trainer = Seq2SeqORTTrainer(
+                            model=model,
+                            args=training_args,
+                            train_dataset=train_dataset,
+                            eval_dataset=valid_dataset,
+                            compute_metrics=compute_metrics if training_args.predict_with_generate else None,
+                            tokenizer=tokenizer,
+                            data_collator=data_collator,
+                            feature="seq2seq-lm",
+                        )
+
+                        train_result = trainer.train()
+                        trainer.save_model()
+                        train_metrics = train_result.metrics
+                        # ort_eval_metrics = trainer.evaluate()
+                        # ort_prediction = trainer.predict(test_dataset)
+                        print("Training metrics(ORT):\n", train_metrics)
+                        # print("Evaluation metrics(ORT):\n", ort_eval_metrics)
+                        # print("Prediction results(ORT):\n", ort_prediction)
 
 
 if __name__ == "__main__":
