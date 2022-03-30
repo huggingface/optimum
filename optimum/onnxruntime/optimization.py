685430f5e942bc55de36f724aee5920aca2b5514
Ella Charlaix <80481427+echarlaix@users.noreply.github.com>
2022-03-30 18:40:17 +0200

    Remove tokenizer pad token attribute setting unrelated with optimization (#125)

--- optimum/onnxruntime/optimization.py
+++ optimum/onnxruntime/optimization.py
@@ -16,7 +16,7 @@ import os
 from pathlib import Path
 from typing import Callable, Dict, List, Optional, Tuple, Union
 
-from transformers import AutoConfig, AutoTokenizer, PreTrainedModel, PreTrainedTokenizer
+from transformers import AutoTokenizer, PreTrainedModel, PreTrainedTokenizer
 from transformers.onnx import export
 from transformers.onnx.features import FeaturesManager
 
@@ -54,15 +54,8 @@ class ORTOptimizer:
             An instance of `ORTOptimizer`.
         """
         tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
-        config = AutoConfig.from_pretrained(model_name_or_path)
-
-        if getattr(config, "pad_token_id", None) is None:
-            config.pad_token_id = tokenizer.eos_token_id
-        if getattr(tokenizer, "pad_token", None) is None:
-            tokenizer.pad_token = tokenizer.eos_token
-
         model_class = FeaturesManager.get_model_class_for_feature(feature)
-        model = model_class.from_pretrained(model_name_or_path, config=config)
+        model = model_class.from_pretrained(model_name_or_path)
         return ORTOptimizer(tokenizer, model, feature, opset)
 
     def __init__(
