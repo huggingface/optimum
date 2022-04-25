7743d5af69a9a807fed4e19e37515b0f19d33e37
Ella Charlaix <ella@huggingface.co>
2022-04-25 16:36:30 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: Apache Software License",
         "Intended Audience :: Developers",
         "Intended Audience :: Education",
@@ -53,8 +53,9 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
-    keywords="transformers, quantization, pruning, training, ipu",
-    url="https://huggingface.co/hardware",
+    keywords="transformers, quantization, pruning, optimization, training, inference, onnx, onnx runtime, "
+    "intel neural compressor, intel habana, graphcore, ipu, hpu",
+    url="https://github.com/huggingface/optimum",
     author="HuggingFace Inc. Special Ops Team",
     author_email="hardware@huggingface.co",
     license="Apache",
