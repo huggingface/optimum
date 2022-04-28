0b8a6cd66bc4147ad75da2ce7862cce44371db1d
Ella Charlaix <ella@huggingface.co>
2022-04-28 12:52:08 +0200

    Fix import

--- examples/onnxruntime/optimization/text-classification/run_glue.py
+++ examples/onnxruntime/optimization/text-classification/run_glue.py
@@ -34,8 +34,8 @@ from transformers import EvalPrediction, HfArgumentParser, PreTrainedTokenizer, 
 from transformers.utils import check_min_version
 from transformers.utils.versions import require_version
 
-from optimum.onnxruntime import ORTModel
 from optimum.onnxruntime.configuration import OptimizationConfig, ORTConfig
+from optimum.onnxruntime.model import ORTModel
 from optimum.onnxruntime.optimization import ORTOptimizer
 
 
