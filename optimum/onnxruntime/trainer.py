55bdfa73b095e94d4aaca7a2cc9458fa3cf87f6e
Jingya HUANG <44135271+JingyaHuang@users.noreply.github.com>
2022-05-11 16:47:42 +0200

    Update optimum/onnxruntime/trainer.py
    Co-authored-by: lewtun <lewis.c.tunstall@gmail.com>

--- optimum/onnxruntime/trainer.py
+++ optimum/onnxruntime/trainer.py
@@ -782,7 +782,7 @@ class ORTTrainer(Trainer):
             logger.info("[INFO] Exporting the model to ONNX...")
             if args.deepspeed and args.fp16:
                 warnings.warn(
-                    "Make sure that `transformers.onnx.export_pytorch` of the transformers verion supports "
+                    "Make sure that `transformers.onnx.export_pytorch` of the transformers version supports "
                     "exporting ONNX on CUDA.",
                     RuntimeWarning,
                 )
