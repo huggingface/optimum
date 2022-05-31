65cc831071bac7de1b6a87cccb385da364884355
lewtun <lewis.c.tunstall@gmail.com>
2022-05-31 15:21:29 +0000

    Remove stuff

--- optimum/utils/preprocessing/__init__.py
+++
@@ -1,3 +0,0 @@
-from .question_answering import QuestionAnsweringProcessing
-from .text_classification import TextClassificationProcessing
-from .token_classification import TokenClassificationProcessing
