8f38e5bdf058510381a0c8d5e63e516ca4790f76
Philipp Schmid <32632186+philschmid@users.noreply.github.com>
2022-06-01 21:06:25 +0200

    fix protobuf

--- setup.py
+++ setup.py
@@ -28,8 +28,8 @@ QUALITY_REQUIRE = ["black~=22.0", "flake8>=3.8.3", "isort>=5.5.4"]
 
 EXTRAS_REQUIRE = {
     #  pip install -e ".[onnxruntime,dev,intel]"  git+https://github.com/huggingface/transformers.git@main --upgrade
-    "onnxruntime": ["onnx", "onnxruntime>=1.9.0", "datasets>=1.2.1"],  # "transformers[sentencepiece]>4.17.0"],
-    "onnxruntime-gpu": ["onnx", "onnxruntime-gpu>=1.9.0", "datasets>=1.2.1"],  # "transformers[sentencepiece]>4.17.0"],
+    "onnxruntime": ["onnx", "onnxruntime>=1.9.0", "datasets>=1.2.1","protobuf==3.20.1"],  # "transformers[sentencepiece]>4.17.0"],
+    "onnxruntime-gpu": ["onnx", "onnxruntime-gpu>=1.9.0", "datasets>=1.2.1","protobuf==3.20.1"],  # "transformers[sentencepiece]>4.17.0"],
     "intel": [
         "pycocotools",
         "neural_compressor>=1.9",
