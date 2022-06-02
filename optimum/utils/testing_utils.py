b55eb75ee6a442292cf735c88cca4f6d405db9e7
JingyaHuang <huang_jingya@outlook.com>
2022-06-02 14:08:31 +0000

    Add decorator for ort training

--- optimum/utils/testing_utils.py
+++ optimum/utils/testing_utils.py
@@ -1,4 +1,7 @@
+import importlib.util
 import os
+import subprocess
+import sys
 import unittest
 
 
@@ -11,3 +14,23 @@ def require_hf_token(test_case):
         return unittest.skip("test requires hf token as `HF_AUTH_TOKEN` environment variable")(test_case)
     else:
         return test_case
+
+
+def require_ort_training(test_case):
+    """
+    Decorator marking a test that requires onnxruntime-training and torch_ort correctly installed and configured.
+    These tests are skipped otherwise.
+    """
+    is_ort_train_available = importlib.util.find_spec("onnxruntime.training") is not None
+
+    if importlib.util.find_spec("torch_ort") is not None:
+        try:
+            is_torch_ort_configured = True
+            subprocess.run([sys.executable, "-m", "torch_ort.configure"], shell=False, check=True)
+        except subprocess.CalledProcessError:
+            is_torch_ort_configured = False
+
+    return unittest.skipUnless(
+        is_ort_train_available and is_torch_ort_configured,
+        "test requires torch_ort correctly installed and configured",
+    )(test_case)
