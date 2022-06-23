90224baeca1bc279347430dead4e29675ef275f0
Felix Marty <felix@huggingface.co>
2022-06-23 16:20:34 +0200

    debug mac

--- tests/benchmark/onnxruntime/testmac_cpu.py
+++ tests/benchmark/onnxruntime/testmac_cpu.py
@@ -3,5 +3,5 @@ import unittest
 
 class TestTextClassification(unittest.TestCase):
     def test_eval_transformers_examples(self):
-        cpu_info = subprocess.check_output(["sysctl -a | grep machdep.cpu"]).decode("utf-8")
+        cpu_info = subprocess.check_output(["sysctl -a | grep machdep.cpu"], shell=True).decode("utf-8")
         print(cpu_info)
