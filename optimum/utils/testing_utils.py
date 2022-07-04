bd20f51d063f2554b2e93ea043217e459f349e18
JingyaHuang <huang_jingya@outlook.com>
2022-07-04 16:29:39 +0000

    Fix sigopt requirement condition

--- optimum/utils/testing_utils.py
+++ optimum/utils/testing_utils.py
@@ -22,7 +22,7 @@ def require_sigopt_token_and_project(test_case):
     """
     use_auth_token = os.environ.get("SIGOPT_API_TOKEN", None)
     has_sigopt_project = os.environ.get("SIGOPT_PROJECT", None)
-    if use_auth_token or has_sigopt_project is None:
+    if use_auth_token is None or has_sigopt_project is None:
         return unittest.skip("test requires an environment variable `SIGOPT_API_TOKEN` and `SIGOPT_PROJECT`")(
             test_case
         )
