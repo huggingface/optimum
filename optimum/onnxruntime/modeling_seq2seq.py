f8d754f7ce6736ef4e7a2d8ccb8ce6238006c670
Ella Charlaix <ella@huggingface.co>
2022-07-11 14:25:33 +0200

    Fix docstring

--- optimum/onnxruntime/modeling_seq2seq.py
+++ optimum/onnxruntime/modeling_seq2seq.py
@@ -384,8 +384,7 @@ class ORTModelForConditionalGeneration(ORTModel):
                 The path to a directory in which a downloaded pretrained model configuration should be cached if the
                 standard cache should not be used.
             use_past_key_values (`bool`, *optional*, defaults to `True`):
-                Whether or not to force the (re-)download of the model weights and configuration files, overriding the
-                cached versions if they exist.
+                Whether or not use the pre-computed key/values hidden-states in order to speed up sequential decoding.
             kwargs (`Dict`, *optional*):
                 kwargs will be passed to the model during initialization.
         """
