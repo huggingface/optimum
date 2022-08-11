3b61e9332aed0edac34c2be0102c73d694f9f278
Felix Marty <9808326+fxmarty@users.noreply.github.com>
2022-08-11 14:43:52 +0200

    fix

--- optimum/fx/optimization/transformations.py
+++ optimum/fx/optimization/transformations.py
@@ -919,6 +919,3 @@ def compose(*args: Transformation, inplace: bool = True) -> Transformation:
                 return ComposeTransformation._reverse_composition(graph_module)
 
     return ComposeTransformation()
-
-def foo():
-    raise ValueError()
