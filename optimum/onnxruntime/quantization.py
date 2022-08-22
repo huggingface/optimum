aef3aff31f6d5c97a68a87e939823aac9fa63eb5
Ella Charlaix <ella@huggingface.co>
2022-08-22 18:00:14 +0200

    Add ort config saving when quantizing a model

--- optimum/onnxruntime/quantization.py
+++ optimum/onnxruntime/quantization.py
@@ -26,7 +26,7 @@ from onnxruntime.quantization import CalibrationDataReader, QuantFormat, Quantiz
 from onnxruntime.quantization.onnx_quantizer import ONNXQuantizer
 from onnxruntime.quantization.qdq_quantizer import QDQQuantizer
 from optimum.onnxruntime import ORTQuantizableOperator
-from optimum.onnxruntime.configuration import CalibrationConfig, NodeName, NodeType, QuantizationConfig
+from optimum.onnxruntime.configuration import CalibrationConfig, NodeName, NodeType, QuantizationConfig, ORTConfig
 from optimum.onnxruntime.modeling_ort import ORTModel
 from optimum.onnxruntime.modeling_seq2seq import ORTModelForConditionalGeneration
 from optimum.onnxruntime.preprocessors import QuantizationPreprocessor
@@ -345,6 +345,10 @@ class ORTQuantizer(OptimumQuantizer):
 
         LOGGER.info(f"Saving quantized model at: {save_dir} (external data format: " f"{use_external_data_format})")
         quantizer.model.save_model_to_file(save_dir, use_external_data_format)
+
+        # Create and save the configuration summarizing all the parameters related to quantization
+        ort_config = ORTConfig(quantization=quantization_config)
+        ort_config.save_pretrained(save_dir)
 
         return Path(save_dir)
 
