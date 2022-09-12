1b99a9f599fb1ac8a310170eb08dd4b35659a69e
Jingya HUANG <44135271+JingyaHuang@users.noreply.github.com>
2022-09-12 17:56:31 +0200

    Add extra dependencies constraints in dockerfiles (#381)

--- examples/onnxruntime/training/docker/run.sh
+++ examples/onnxruntime/training/docker/run.sh
@@ -12,14 +12,11 @@ GPU_DEVICES=${2:-"all"}
 # docker run -it --rm -p 80:8888 --gpus $GPU_DEVICES ort9/cu11:latest $CMD
 
 # Install dependencies
-pip install -U pip
-pip install pygit2 pgzip
 # Install transformers from source as there was a bug on the 4.21.0 release: https://github.com/huggingface/transformers/issues/18350
 pip install git+https://github.com/huggingface/transformers
 pip install datasets accelerate #transformers
 pip install coloredlogs absl-py rouge_score seqeval scipy sacrebleu nltk sklearn parameterized sentencepiece
 pip install fairscale deepspeed mpi4py
-pip install --upgrade protobuf==3.20.1
 # pip install optuna ray sigopt wandb # Hyper parameter search
 
 # Install apex
