2a073c56efc464f00fea0e89c8810bd8f9baced5
JingyaHuang <huang_jingya@outlook.com>
2022-09-13 07:39:39 +0000

    Fix style of args doc

--- optimum/onnxruntime/training_args_seq2seq.py
+++ optimum/onnxruntime/training_args_seq2seq.py
@@ -25,10 +25,8 @@ from .training_args import ORTTrainingArguments
 class ORTSeq2SeqTrainingArguments(Seq2SeqTrainingArguments, ORTTrainingArguments):
     """
     Parameters:
-        optim (`str` or [`training_args.ORTOptimizerNames`] or [`transformers.training_args.OptimizerNames`], *optional*,
-        defaults to `"adamw_hf"`):
-            The optimizer to use, including optimizers in Transformers: adamw_hf, adamw_torch, adamw_apex_fused, or adafactor.
-            And optimizers implemented by ONNX Runtime: adamw_ort_fused.
+        optim (`str` or [`training_args.ORTOptimizerNames`] or [`transformers.training_args.OptimizerNames`], *optional*, defaults to `"adamw_hf"`):
+            The optimizer to use, including optimizers in Transformers: adamw_hf, adamw_torch, adamw_apex_fused, or adafactor. And optimizers implemented by ONNX Runtime: adamw_ort_fused.
     """
 
     optim: Optional[str] = field(
