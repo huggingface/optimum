e63a030890f6fbed1c23928a325a7425297a5244
Michael Benayoun <mickbenayoun@gmail.com>
2022-09-27 17:53:22 +0200

    Update file headers

--- optimum/exporters/onnx/__main__.py
+++ optimum/exporters/onnx/__main__.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Entry point to the optimum.exporters.onnx command line."""
+
 from argparse import ArgumentParser
 from pathlib import Path
 
--- optimum/exporters/onnx/base.py
+++ optimum/exporters/onnx/base.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Onnx configuration base classes."""
+
 import copy
 import dataclasses
 import itertools
