399a344fae4810b8a53c260003bd70f76445c5ce
JingyaHuang <huang_jingya@outlook.com>
2022-10-09 21:54:51 +0000

    Update trfrs minimal version in examples

--- examples/onnxruntime/training/language-modeling/run_clm.py
+++ examples/onnxruntime/training/language-modeling/run_clm.py
@@ -51,7 +51,8 @@ import evaluate
 from optimum.onnxruntime import ORTTrainer, ORTTrainingArguments
 
 
-check_min_version("4.22.0")
+# Will error if the minimal version of Transformers is not installed. Remove at your own risks.
+check_min_version("4.23.0.dev0")
 
 require_version(
     "datasets>=1.8.0", "To fix: pip install -r examples/onnxruntime/training/language-modeling/requirements.txt"
