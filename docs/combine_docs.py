56cccd25fc1c8adcadfa02c55a05f848f14d73a9
regisss <15324346+regisss@users.noreply.github.com>
2022-10-25 18:53:17 +0200

    Change the logic to combine subpackages' documentation (#429)

--- docs/combine_docs.py
+++ docs/combine_docs.py
@@ -20,7 +20,7 @@ parser.add_argument("--version", type=str, default="main", help="The version of 
 
 def rename_subpackage_toc(subpackage: str, toc: Dict):
     """
-    Extend table of contents sections with subpackage name prefix.
+    Extend table of contents sections with the subpackage name as the parent folder.
 
     Args:
         subpackage (str): subpackage name.
@@ -29,21 +29,73 @@ def rename_subpackage_toc(subpackage: str, toc: Dict):
     for item in toc:
         for file in item["sections"]:
             if "local" in file:
-                file["local"] = f"{subpackage}_" + file["local"]
+                file["local"] = f"{subpackage}/" + file["local"]
             else:
                 # if "local" is not in file, it means we have a subsection, hence the recursive call
                 rename_subpackage_toc(subpackage, [file])
 
 
+def rename_copy_subpackage_html_paths(subpackage: str, subpackage_path: Path, optimum_path: Path, version: str):
+    """
+    Copy and rename the files from the given subpackage's documentation to Optimum's documentation.
+    In Optimum's documentation, the subpackage files are organized as follows:
+
+    optimum_doc
+        ├──────── subpackage_1
+        │               ├────── folder_1
+        │               │          └───── ...
+        │               ├────── ...
+        │               └────── folder_x
+        │                          └───── ...
+        ├──────── ...
+        ├──────── subpackage_n
+        │               ├────── folder_1
+        │               │          └───── ...
+        │               ├────── ...
+        │               └────── folder_y
+        │                          └───── ...
+        └──────── usual_optimum_doc
+
+    Args:
+        subpackage (str): subpackage name
+        subpackage_path (Path): path to the subpackage's documentation
+        optimum_path (Path): path to Optimum's documentation
+        version (str): Optimum's version
+    """
+    subpackage_html_paths = list(subpackage_path.rglob("*.html"))
+    # The language folder is the 4th folder in the subpackage HTML paths
+    language_folder_level = 3
+
+    for html_path in subpackage_html_paths:
+        language_folder = html_path.parts[language_folder_level]
+        # Build the relative path from the language folder
+        relative_path_from_language_folder = Path(*html_path.parts[language_folder_level + 1 :])
+        # New path in Optimum's doc
+        new_path_in_optimum = Path(
+            f"{optimum_path}/optimum/{version}/{language_folder}/{subpackage}/{relative_path_from_language_folder}"
+        )
+        # Build the parent folders if necessary
+        new_path_in_optimum.parent.mkdir(parents=True, exist_ok=True)
+
+        shutil.copyfile(html_path, new_path_in_optimum)
+
+
 def main():
     args = parser.parse_args()
     optimum_path = Path("optimum-doc-build")
+
+    # Copy and rename all files from subpackages' docs to Optimum doc
     for subpackage in args.subpackages:
         subpackage_path = Path(f"{subpackage}-doc-build")
+
         # Copy all HTML files from subpackage into optimum
-        subpackage_html_paths = list(subpackage_path.rglob("*.html"))
-        for html_path in subpackage_html_paths:
-            shutil.copyfile(html_path, f"{optimum_path}/optimum/{args.version}/en/{subpackage}_{html_path.name}")
+        rename_copy_subpackage_html_paths(
+            subpackage,
+            subpackage_path,
+            optimum_path,
+            args.version,
+        )
+
         # Load optimum table of contents
         base_toc_path = next(optimum_path.rglob("_toctree.yml"))
         with open(base_toc_path, "r") as f:
@@ -52,7 +104,7 @@ def main():
         subpackage_toc_path = next(subpackage_path.rglob("_toctree.yml"))
         with open(subpackage_toc_path, "r") as f:
             subpackage_toc = yaml.safe_load(f)
-        # Extend table of contents sections with subpackage name prefix
+        # Extend table of contents sections with the subpackage name as the parent folder
         rename_subpackage_toc(subpackage, subpackage_toc)
         # Update optimum table of contents
         base_toc.extend(subpackage_toc)
