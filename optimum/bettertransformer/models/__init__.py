fb16afa06c0f48cfe9700a7e0cfb2983e1bb3d48
younesbelkada <younesbelkada@gmail.com>
2022-11-02 13:35:58 +0000

    remove `t5`
    - `t5` is not supported due to the attention bias

--- optimum/bettertransformer/models/__init__.py
+++ optimum/bettertransformer/models/__init__.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from . import bart, bert, distilbert, t5
+from . import bart, bert, distilbert
 
 
 FAST_LAYERS_MAPPING_DICT = {
