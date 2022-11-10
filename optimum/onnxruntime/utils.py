ffa58125ec81705146a77b694286cccea7d9572e
Michael Benayoun <mickbenayoun@gmail.com>
2022-11-10 16:25:40 +0100

    Update tests

--- optimum/onnxruntime/utils.py
+++ optimum/onnxruntime/utils.py
@@ -87,7 +87,7 @@ class ORTConfigManager:
         "gpt_neo": (NormalizedTextConfig.with_args(num_attention_heads="num_heads"), "gpt2"),
         "marian": (BartLikeNormalizedTextConfig, "bart"),
         "mbart": (BartLikeNormalizedTextConfig, "bart"),
-        "mt5": (T5LikeNormalizedTextConfig, "t5"),
+        "mt5": (T5LikeNormalizedTextConfig, "bart"),
         "m2m_100": (BartLikeNormalizedTextConfig, "bart"),
         "roberta": (NormalizedTextConfig, "bert"),
         "t5": (T5LikeNormalizedTextConfig, "t5"),
@@ -102,7 +102,7 @@ class ORTConfigManager:
     @classmethod
     def get_model_ort_type(cls, model_type: str) -> str:
         cls.check_supported_model(model_type)
-        return cls._conf[model_type][2]
+        return cls._conf[model_type][1]
 
     @classmethod
     def check_supported_model(cls, model_type: str):
@@ -116,7 +116,7 @@ class ORTConfigManager:
     @classmethod
     def check_optimization_supported_model(cls, model_type: str):
         supported_model_types_for_optimization = ["bert", "gpt2", "bart"]
-        if (model_type not in cls._conf) or (cls._conf[model_type][2] not in supported_model_types_for_optimization):
+        if (model_type not in cls._conf) or (cls._conf[model_type][1] not in supported_model_types_for_optimization):
             raise KeyError(
                 f"ONNX Runtime doesn't support the graph optimization of {model_type} yet. Only {supported_model_types_for_optimization} are supported. "
                 f"If you want to support {model_type} please propose a PR or open up an issue in ONNX Runtime:https://github.com/microsoft/onnxruntime."
