1588a2ec986306bd804c42ad80224d09ce42a343
fxmarty <9808326+fxmarty@users.noreply.github.com>
2022-12-08 12:28:39 +0100

    Add CLIP BetterTransformer (#534)
    * add clip
    
    * style and test
    
    * better support
    
    * debug
    
    * style
    
    * support clip vision model
    
    * typo
    
    * fix test
    
    * fix test
    
    * add clip in doc
    
    * fix test
    
    * remove comment
    
    * Update optimum/bettertransformer/models/encoder_models.py
    
    Co-authored-by: Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
    
    * fix style
    
    Co-authored-by: Younes Belkada <49240599+younesbelkada@users.noreply.github.com>

--- optimum/utils/testing_utils.py
+++ optimum/utils/testing_utils.py
@@ -1,3 +1,4 @@
+import collections
 import importlib.util
 import itertools
 import os
@@ -9,6 +10,20 @@ from typing import Any, Dict, Iterable
 from packaging import version
 
 from optimum.utils import is_accelerate_available
+
+
+def flatten_dict(dictionary: Dict):
+    """
+    Flatten a nested dictionaries as a flat dictionary.
+    """
+    items = []
+    for k, v in dictionary.items():
+        new_key = k
+        if isinstance(v, collections.MutableMapping):
+            items.extend(flatten_dict(v).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
 
 
 def require_accelerate(test_case):
