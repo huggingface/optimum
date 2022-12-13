ec06b9c065328d71e9777258b8b126f36bb870b7
fxmarty <9808326+fxmarty@users.noreply.github.com>
2022-12-13 10:14:51 +0100

    More robust tests for ORTModel using decoders and use_cache=True (#576)
    * more robust tests for ORTModel using decoders and use_cache=True
    
    * keep tuple
    
    * Update optimum/utils/testing_utils.py
    
    Co-authored-by: Michael Benayoun <mickbenayoun@gmail.com>
    
    * add test name everywhere when using grid_parameters
    
    * nit
    
    * nit doc
    
    * nit in method signatures
    
    Co-authored-by: Michael Benayoun <mickbenayoun@gmail.com>

--- optimum/utils/testing_utils.py
+++ optimum/utils/testing_utils.py
@@ -126,7 +126,8 @@ def convert_to_hf_classes(mapping_dict):
 
 def grid_parameters(parameters: Dict[str, Iterable[Any]]) -> Iterable[Dict[str, Any]]:
     """
-    Generate an iterable over the grid of all combinations of parameters
+    Generates an iterable over the grid of all combinations of parameters, adding a test name as the first item in the yielded list
     """
     for params in itertools.product(*parameters.values()):
-        yield list(params)
+        test_name = "_".join([str(param) for param in params])
+        yield [test_name] + list(params)
