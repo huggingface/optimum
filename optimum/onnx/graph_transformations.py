0296ca5675082f12c77c471aaced870066ae7f95
JingyaHuang <huang_jingya@outlook.com>
2022-12-14 00:28:13 +0000

    Add merge function

--- optimum/onnx/graph_transformations.py
+++ optimum/onnx/graph_transformations.py
@@ -13,10 +13,12 @@
 #  limitations under the License.
 import copy
 from collections import defaultdict
-from typing import DefaultDict, Dict, Set, Tuple
+from typing import DefaultDict, Dict, List, Set, Tuple
+
+import numpy as np
 
 import onnx
-from onnx import ModelProto
+from onnx import ModelProto, ValueInfoProto
 
 
 def _find_duplicate_weights(model) -> DefaultDict[Tuple[int, bytes], Set[str]]:
@@ -112,35 +114,78 @@ def _replace_atenops_to_gather(model: ModelProto):
     return model
 
 
-def _trace_upsteam_nodes(graph, name, node_input_names, node_map, initializer_map):
+def _infer_output_shape(output: ValueInfoProto):
+    output_shape = []
+    for dim in output.type.tensor_type.shape.dim:
+        if getattr(dim, "dim_param"):
+            output_shape.append(getattr(dim, "dim_param"))
+        elif getattr(dim, "dim_value"):
+            output_shape.append(getattr(dim, "dim_value"))
+        else:
+            raise ValueError(f"Can not find `dim_param` nor `dim_value` in the output dimension info.")
+
+    return output_shape
+
+
+def _check_num_outputs(model1: ModelProto, model2: ModelProto):
+    if not len(model1.graph.output) == len(model2.graph.output):
+        raise ValueError(
+            f"Two model protos need to have same outputs. But one has {len(model1.graph.output)} "
+            f"outputs while the other has {len(model2.graph.output)} outputs."
+        )
+
+
+def _unify_onnx_outputs(model1: ModelProto, model2: ModelProto):
     """
-    Recurisvely traces all upstream nodes of a particular node in the graph.
-
-    Args:
-        decoder (`~onnx.ModelProto`): The decoder ONNX model.
-        decoder_with_past (`~onnx.ModelProto`): The decoder with past ONNX model.
-
-    Returns:
-        `~onnx.ModelProto`: The fused decoder ONNX model.
+    Unify the outputs of deux ONNX model protos. The outputs of model1 will be replaced by outputs of model2.
     """
-    for n in graph.node:
-        for noutput in n.output:
-            if (noutput == name) and (n.name not in node_input_names):
-                # give node "name" is node n's output, so add node "n" to node_input_names list
-                node_input_names.append(n.name)
-                if n.name in node_map.keys():
-                    for ninput in node_map[n.name].input:
-                        # trace input node's inputs
-                        node_input_names = _trace_upsteam_nodes(
-                            graph, ninput, node_input_names, node_map, initializer_map
-                        )
-    # Check if it is initializer which can be terminal inputs on a path.
-    if name in initializer_map.keys():
-        node_input_names.append(name)
-    return node_input_names
+    _check_num_outputs(model1, model2)
+
+    for idx in range(len(model1.graph.output)):
+        model_output_1 = model1.graph.output[idx]
+        model_output_2 = model2.graph.output[idx]
+        if not model_output_1 == model_output_2:
+            if not (
+                model_output_1.name == model_output_2.name
+                and model_output_1.type.tensor_type.elem_type == model_output_2.type.tensor_type.elem_type
+            ):
+                raise ValueError(
+                    f"Can not match {model_output_1.name} with {model_output_2.name}. Make sure your"
+                    f" model protos have same outputs, have same data types and are in the same order."
+                )
+            model1.graph.output.remove(model_output_1)
+
+            new_output = onnx.helper.make_tensor_value_info(
+                model_output_2.name,
+                model_output_2.type.tensor_type.elem_type,
+                _infer_output_shape(model_output_2),
+            )
+            model1.graph.output.insert(idx, new_output)
+
+    if not all(
+        model_output_1 == model_output_2
+        for model_output_1, model_output_2 in zip(model1.graph.output, model2.graph.output)
+    ):
+        raise RuntimeError(f"Failed to unify outputs of given ONNX model protos.")
 
 
-def fuse_decoders(decoder: ModelProto, decoder_with_past: ModelProto):
+def _get_all_inputs(model_list: List[ModelProto]):
+    inputs = []
+    input_names = []
+    for model in model_list:
+        for input in model.graph.input:
+            if input.name not in input_names:
+                input_names.append(input.name)
+                inputs.append(input)
+    return inputs
+
+
+def merge_decoders(
+    decoder: ModelProto,
+    decoder_with_past: ModelProto,
+    model_name: str = "merged",
+    producer_name: str = "optimum-onnx",
+) -> ModelProto:
     """
     Fuses decoder ONNX model and decoder with past ONNX model into one ONNX model with if logic.
 
@@ -151,4 +196,45 @@ def fuse_decoders(decoder: ModelProto, decoder_with_past: ModelProto):
     Returns:
         `~onnx.ModelProto`: The fused decoder ONNX model.
     """
-    pass
+    _unify_onnx_outputs(decoder, decoder_with_past)
+    all_inputs = _get_all_inputs([decoder, decoder_with_past])
+
+    # Make subgraphs
+    no_past_branch = onnx.helper.make_graph(
+        nodes=decoder.graph.node,
+        name="no_past",
+        inputs=[],
+        outputs=decoder.graph.output,
+        initializer=decoder.graph.initializer,
+    )
+    with_past_branch = onnx.helper.make_graph(
+        nodes=decoder_with_past.graph.node,
+        name="with_past",
+        inputs=[],
+        outputs=decoder_with_past.graph.output,
+        initializer=decoder_with_past.graph.initializer,
+    )
+
+    # Merge subgraphs with a `If` node
+    use_cache = onnx.helper.make_tensor_value_info(
+        "use_cache",
+        onnx.TensorProto.BOOL,
+        [1],
+    )
+    if_node = onnx.helper.make_node(
+        "If",
+        inputs=["use_cache"],
+        outputs=[output.name for output in no_past_branch.output],
+        then_branch=with_past_branch,
+        else_branch=no_past_branch,
+    )
+    merged_graph = onnx.helper.make_graph(
+        [if_node],
+        model_name,
+        all_inputs + [use_cache],
+        no_past_branch.output,
+    )
+    merged_model = onnx.helper.make_model(merged_graph, producer_name=producer_name)
+    onnx.checker.check_model(merged_model)
+
+    return merged_model
