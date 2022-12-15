f708b5873fac786c9597a8c3049b24a077f52d06
NouamaneTazi <nouamane98@gmail.com>
2022-12-15 19:13:28 +0100

    didn't work, reverting "try saving to single file"
    This reverts commit 0fdfd873ca6b55db1083365f69f783a7d9d4be49.

--- optimum/exporters/onnx/convert.py
+++ optimum/exporters/onnx/convert.py
@@ -31,8 +31,6 @@ from .utils import (
     is_torch_onnx_support_available,
 )
 
-from ...onnxruntime.utils import check_model_uses_external_data
-
 
 if is_torch_available():
     from transformers.modeling_utils import PreTrainedModel
@@ -325,20 +323,6 @@ def export_pytorch(
                 do_constant_folding=True,
                 opset_version=opset,
             )
-
-            # check if external data was exported
-            import onnx
-            onnx_model = onnx.load(str(output), load_external_data=False)
-            model_uses_external_data = check_model_uses_external_data(onnx_model)
-
-            from onnx.external_data_helper import convert_model_to_external_data
-
-            if True:
-                logger.info("Saving external data to one file...")
-                convert_model_to_external_data(onnx_model, all_tensors_to_one_file=True, size_threshold=0, convert_attribute=False)
-                onnx.save(onnx_model, str(output),
-                save_as_external_data=True, all_tensors_to_one_file=True,
-                size_threshold=0, convert_attribute=False)
 
         config.restore_ops()
 
