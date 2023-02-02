259b6b6d070f05be241b1733922b2667ef66c888
Félix Marty <9808326+fxmarty@users.noreply.github.com>
2023-02-02 15:21:14 +0100

    add script

---
+++ research/stable_diffusion_end_to_end_onnx/create_scriptmodule_by_tracing.py
@@ -0,0 +1,110 @@
+from optimum.exporters.onnx.utils import get_stable_diffusion_models_for_export
+from transformers import AutoTokenizer, CLIPTextConfig
+
+from diffusers.schedulers import PNDMScheduler
+
+from diffusers import DiffusionPipeline
+import torch
+
+from scriptable_pipeline_stable_diffusion import ScriptableStableDiffusionPipeline
+
+from utils import export_models, get_traced_submodules, StableDiffusionPreprocessor
+
+from schedulers.scheduling_pndm import ScriptablePNDMScheduler
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--gpu",
+    action="store_true",
+    help="use to trace and script on GPU.",
+)
+args = parser.parse_args()
+
+model_name = "CompVis/stable-diffusion-v1-4"
+#model_name = "hf-internal-testing/tiny-stable-diffusion-torch"
+pipeline = DiffusionPipeline.from_pretrained(model_name, low_cpu_mem_usage=False)
+
+num_inference_steps = 50
+
+if args.gpu:
+    device= "cuda"
+else:
+    device = "cpu"
+
+scriptable_scheduler = ScriptablePNDMScheduler(**pipeline.scheduler.config)
+scriptable_scheduler.set_timesteps(num_inference_steps, device=device)
+
+pipeline = ScriptableStableDiffusionPipeline(
+    vae=pipeline.vae,
+    text_encoder=pipeline.text_encoder,
+    tokenizer=pipeline.tokenizer,
+    unet=pipeline.unet,
+    scheduler=scriptable_scheduler,
+)
+
+if args.gpu:
+    pipeline = pipeline.to(device)
+
+models_and_onnx_configs = get_stable_diffusion_models_for_export(pipeline)
+
+in_dummy_out_per_model = export_models(
+    models_and_onnx_configs=models_and_onnx_configs,
+)
+
+print("Starting tracing...")
+with torch.inference_mode():
+    text_encoder_traced, unet_traced, vae_decoder_traced = get_traced_submodules(
+        in_dummy_out_per_model, models_and_onnx_configs
+    )
+print("Tracing done.")
+
+script = True
+
+if script:
+    pipeline.vae_decoder = vae_decoder_traced
+    pipeline.vae = None  # the VAE is not scriptable
+else:
+    pipeline.vae.decode = lambda latents, return_dict: vae_decoder_traced(latents)
+pipeline.unet = unet_traced
+pipeline.text_encoder = text_encoder_traced
+
+# now trace the whole pipeline
+text_encoder_config = CLIPTextConfig.from_pretrained(model_name, subfolder="text_encoder")
+tokenizer = AutoTokenizer.from_pretrained(model_name, subfolder="tokenizer")
+scheduler = PNDMScheduler.from_pretrained(model_name, subfolder="scheduler")
+
+preprocessor = StableDiffusionPreprocessor(
+    tokenizer,
+    text_encoder_config,
+    do_classifier_free_guidance=True,
+    scheduler=scheduler,
+)
+
+num_inference_steps = 50
+preprocessed_input = preprocessor.preprocess("A cat sleeping on the beach", num_inference_steps=num_inference_steps, device=device)
+
+text_input_ids = preprocessed_input["text_input_ids"].to(device)
+uncond_text_input_ids = preprocessed_input["uncond_text_input_ids"].to(device)
+timesteps = preprocessed_input["timesteps"].to(device)
+
+
+with torch.inference_mode():
+    traced_pipeline = torch.jit.trace(pipeline, (text_input_ids, uncond_text_input_ids, timesteps))
+
+print(traced_pipeline.code)
+
+#print("decode_latent:")
+#print(traced_pipeline.decode_latents.code)
+
+print("unet:")
+print(traced_pipeline.unet.code)
+
+#print("scheduler:")
+#print(traced_pipeline.scheduler.step.code)
+
+#print("scheduler plms:")
+#print(traced_pipeline.scheduler.step_plms.code)
+
+print("Saving...")
+torch.jit.save(traced_pipeline, f"traced_sd_{device}.pt")
