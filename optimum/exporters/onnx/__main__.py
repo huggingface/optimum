87ab5a3e75f1f3bd31fe5cdffa0cbec8ac6ee7d7
fxmarty <9808326+fxmarty@users.noreply.github.com>
2023-02-06 14:37:28 +0100

    Update optimum/exporters/onnx/__main__.py
    Co-authored-by: Michael Benayoun <mickbenayoun@gmail.com>

--- optimum/exporters/onnx/__main__.py
+++ optimum/exporters/onnx/__main__.py
@@ -68,7 +68,7 @@ def main():
         task, args.model, framework=args.framework, cache_dir=args.cache_dir, trust_remote_code=args.trust_remote_code
     )
 
-    # make -with-past the default if --task was not explicitely specified
+    # Make -with-past the default if --task was not explicitely specified
     if (
         args.task == "auto"
         and task in ["seq2seq-lm", "causal-lm", "speech2seq-lm", "vision2seq-lm"]
