2fde7620e2270c8d58df05d31b8dcf34a05b3e6a
Félix Marty <9808326+fxmarty@users.noreply.github.com>
2023-02-13 09:50:08 +0100

    disable provider check on windows

--- optimum/onnxruntime/utils.py
+++ optimum/onnxruntime/utils.py
@@ -176,9 +176,7 @@ def validate_provider_availability(provider: str):
         provider (str): Name of an ONNX Runtime execution provider.
     """
     # disable on Windows as reported in https://github.com/huggingface/optimum/issues/769
-    if os.name == "nt":
-        return
-    if provider in ["CUDAExecutionProvider", "TensorrtExecutionProvider"]:
+    if os.name != "nt" and provider in ["CUDAExecutionProvider", "TensorrtExecutionProvider"]:
         path_cuda_lib = os.path.join(ort.__path__[0], "capi", "libonnxruntime_providers_cuda.so")
         path_trt_lib = os.path.join(ort.__path__[0], "capi", "libonnxruntime_providers_tensorrt.so")
         path_dependecy_loading = os.path.join(ort.__path__[0], "capi", "_ld_preload.py")
