7db2e17f5026cfa0a9da8c270539d039a1d68812
Félix Marty <9808326+fxmarty@users.noreply.github.com>
2023-02-16 16:53:20 +0100

    fix

---
+++ research/stable_diffusion_end_to_end_onnx/run_original_pipeline.py
@@ -0,0 +1,53 @@
+from diffusers import DiffusionPipeline
+import argparse
+import torch
+import time
+from constants import MODEL_NAME
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--gpu",
+    action="store_true",
+    help="use to trace and script on GPU.",
+)
+args = parser.parse_args()
+
+if args.gpu:
+    device = "cuda"
+else:
+    device = "cpu"
+
+# NOTE: Beware that model_path should match with the .pt model!
+model_path = MODEL_NAME
+
+pipeline = DiffusionPipeline.from_pretrained(model_path)
+
+num_inference_steps = 50
+num_images_per_prompt = 2
+width = 512
+height = 512
+guidance_scale = 7.5
+
+with torch.inference_mode():
+    print("FORWARD")
+    res = pipeline(
+        "A cat sleeping on the beach",
+        num_inference_steps=num_inference_steps,
+        width=width,
+        height=height,
+        guidance_scale=guidance_scale,
+        num_images_per_prompt=num_images_per_prompt,
+    )
+
+    for i in range(5):
+        print("FORWARD")
+        start = time.time()
+        torch_images = pipeline(
+            "A cat sleeping on the beach",
+            num_inference_steps=num_inference_steps,
+            width=width,
+            height=height,
+            guidance_scale=guidance_scale,
+            num_images_per_prompt=num_images_per_prompt,
+        )[0]
+        print(f"Took {time.time() - start} s")
