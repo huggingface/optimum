ef51c85919c6eef77508174ab834ea9977437c84
Félix Marty <9808326+fxmarty@users.noreply.github.com>
2023-03-01 14:23:42 +0100

    docbuilder does not seem to support softlinks in titles

--- optimum/onnxruntime/modeling_ort.py
+++ optimum/onnxruntime/modeling_ort.py
@@ -621,16 +621,16 @@ class ORTModel(OptimizedModel):
         kwargs (`Dict[str, Any]`):
             Will be passed to the underlying model loading methods.
 
-        > Parameters for decoder models ([`~onnxruntime.ORTModelForCausalLM`], [`~onnxruntime.ORTModelForSeq2SeqLM`], [`~onnxruntime.ORTModelForSeq2SeqLM`], [`~onnxruntime.ORTModelForSpeechSeq2Seq`], [`~onnxruntime.ORTModelForVision2Seq`])
+        > Parameters for decoder models (ORTModelForCausalLM, ORTModelForSeq2SeqLM, ORTModelForSeq2SeqLM, ORTModelForSpeechSeq2Seq, ORTModelForVision2Seq)
 
         use_cache (`Optional[bool]`, defaults to `True`):
             Whether or not past key/values cache should be used. Defaults to `True`.
 
-        > Parameters for [`~onnxruntime.ORTModelForCausalLM`]
+        > Parameters for ORTModelForCausalLM
 
         use_merged (`Optional[bool]`, defaults to `None`):
             whether or not to use a single ONNX that handles both the decoding without and with past key values reuse. This option defaults
-            to `True` if a merged decoder is found in the repository, to `False` otherwise. When exporting with `from_transformers=True`,
+            to `True` if loading from a local repository and a merged decoder is found. When exporting with `from_transformers=True`,
             defaults to `False`. This option should be set to `True` to minimize memory usage.
 
         Returns:
