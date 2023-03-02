bd946d2a8bcb7dff614e6cd21cccb125d3d4f9de
JingyaHuang <jingya@huggingface.co>
2023-03-02 15:59:51 +0000

    lazy

--- optimum/exporters/__init__.py
+++ optimum/exporters/__init__.py
@@ -13,4 +13,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from . import onnx  # noqa
-from .tasks import TasksManager  # noqa
+
+from typing import TYPE_CHECKING
+
+from transformers.utils import _LazyModule
+
+
+_import_structure = {
+    "tasks": ["TasksManager"],
+}
+
+if TYPE_CHECKING:
+    from .tasks import TasksManager  # noqa
+else:
+    import sys
+
+    sys.modules[__name__] = _LazyModule(
+        __name__,
+        globals()["__file__"],
+        _import_structure,
+        module_spec=__spec__,
+    )
