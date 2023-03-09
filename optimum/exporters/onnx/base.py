46b27737a8da6826e9eb83d21faa8df4813e2a23
Félix Marty <9808326+fxmarty@users.noreply.github.com>
2023-03-09 16:16:19 +0100

    fix ort implementation

--- optimum/exporters/onnx/base.py
+++ optimum/exporters/onnx/base.py
@@ -15,16 +15,14 @@
 """ONNX configuration base classes."""
 
 import copy
-import dataclasses
 import enum
-import functools
 import gc
 import inspect
 import itertools
 import re
 from abc import ABC, abstractmethod
 from collections import OrderedDict
-from typing import TYPE_CHECKING, Any, Callable, Dict, Iterable, List, Optional, Tuple, Union
+from typing import TYPE_CHECKING, Any, Dict, Iterable, List, Optional, Tuple, Union
 
 import numpy as np
 import onnx
@@ -41,6 +39,7 @@ from ...utils import (
 from ...utils import TORCH_MINIMUM_VERSION as GLOBAL_MIN_TORCH_VERSION
 from ...utils.doc import add_dynamic_docstring
 from ..base import ExportConfig
+from .model_patcher import ModelPatcher, Seq2SeqModelPatcher
 
 
 if TYPE_CHECKING:
@@ -51,87 +50,9 @@ if TYPE_CHECKING:
     if is_diffusers_available():
         from diffusers import ModelMixin
 
+    from .model_patcher import PatchingSpec
 
 logger = logging.get_logger(__name__)
-
-
-@dataclasses.dataclass
-class PatchingSpec:
-    """
-    Data class that holds patching specifications.
-
-    Args:
-        o: Module / object where the op to patch is located
-        name: Name of the op to monkey patch
-        custom_op: Custom op that patches the original op
-        orig_op: Original op that is being patched
-        op_wrapper: Wrapper (optional) that wraps both the original and custom ops.
-            It is useful for ops that are class or static methods for instance.
-    """
-
-    o: Any
-    name: str
-    custom_op: Callable
-    orig_op: Optional[Callable] = None
-    op_wrapper: Optional[Callable] = None
-
-
-class ModelPatcher:
-    def __init__(self, config: "OnnxConfig", model: Union["PreTrainedModel", "TFPreTrainedModel"]):
-        self._model = model
-
-        patching_specs = config.PATCHING_SPECS
-        self._patching_specs = []
-        for spec in patching_specs if patching_specs is not None else []:
-            final_spec = spec
-            if spec.orig_op is None:
-                final_spec = dataclasses.replace(spec, orig_op=getattr(spec.o, spec.name))
-            self._patching_specs.append(final_spec)
-
-        self.orig_forward_name = "forward" if hasattr(self._model, "forward") else "call"
-        self.orig_forward = getattr(self._model, self.orig_forward_name)
-
-        # TODO: remove that once we got rid of OnnxConfigWithLoss or we implemented it better.
-        if isinstance(config, OnnxConfigWithLoss):
-            real_config = config._onnx_config
-        else:
-            real_config = config
-        allow_past_in_outputs = isinstance(real_config, OnnxConfigWithPast) and real_config.use_present_in_outputs
-
-        @functools.wraps(self.orig_forward)
-        def patched_forward(*args, **kwargs):
-            outputs = self.orig_forward(*args, **kwargs)
-            return {
-                k: v
-                for k, v in outputs.items()
-                if config.torch_to_onnx_output_map.get(k, k) in config.outputs
-                or (allow_past_in_outputs and k.startswith("past_key_values"))
-            }
-
-        self.patched_forward = patched_forward
-
-    def patch_ops(self):
-        for spec in self._patching_specs:
-            custom_op = spec.custom_op if spec.op_wrapper is None else spec.op_wrapper(spec.custom_op)
-            setattr(spec.o, spec.name, custom_op)
-
-    def restore_ops(self):
-        for spec in self._patching_specs:
-            orig_op = spec.orig_op if spec.op_wrapper is None else spec.op_wrapper(spec.orig_op)
-            setattr(spec.o, spec.name, orig_op)
-
-    def __enter__(self):
-        self.patch_ops()
-        setattr(self._model, self.orig_forward_name, self.patched_forward)
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.restore_ops()
-        setattr(self._model, self.orig_forward_name, self.orig_forward)
-
-    def __call__(self, *args, **kwargs):
-        if getattr(self._model, self.orig_forward_name) is self.orig_forward:
-            logger.warning("Running the non-patched model")
-        return self._model(*args, **kwargs)
 
 
 GENERATE_DUMMY_DOCSTRING = r"""
@@ -196,7 +117,7 @@ class OnnxConfig(ExportConfig, ABC):
     DEFAULT_ONNX_OPSET = 11
     ATOL_FOR_VALIDATION: Union[float, Dict[str, float]] = 1e-5
     MIN_TORCH_VERSION = GLOBAL_MIN_TORCH_VERSION
-    PATCHING_SPECS: Optional[List[PatchingSpec]] = None
+    PATCHING_SPECS: Optional[List["PatchingSpec"]] = None
     _TASK_TO_COMMON_OUTPUTS = {
         "audio-classification": OrderedDict({"logits": {0: "batch_size"}}),
         "audio-frame-classification": OrderedDict({"logits": {0: "batch_size", 1: "sequence_length"}}),
@@ -785,8 +706,10 @@ class OnnxSeq2SeqConfigWithPast(OnnxConfigWithPast):
         for i in range(self._normalized_config.decoder_num_layers):
             inputs_or_outputs[f"{name}.{i}.decoder.key"] = {0: "batch_size", 2: decoder_sequence}
             inputs_or_outputs[f"{name}.{i}.decoder.value"] = {0: "batch_size", 2: decoder_sequence}
-            inputs_or_outputs[f"{name}.{i}.encoder.key"] = {0: "batch_size", 2: encoder_sequence}
-            inputs_or_outputs[f"{name}.{i}.encoder.value"] = {0: "batch_size", 2: encoder_sequence}
+
+            if direction == "inputs" or (self._behavior is ConfigBehavior.DECODER and self.use_past is False):
+                inputs_or_outputs[f"{name}.{i}.encoder.key"] = {0: "batch_size", 2: encoder_sequence}
+                inputs_or_outputs[f"{name}.{i}.encoder.value"] = {0: "batch_size", 2: encoder_sequence}
 
         if direction == "outputs" and "encoder_last_hidden_state" in inputs_or_outputs:
             inputs_or_outputs.move_to_end("encoder_last_hidden_state")
@@ -796,6 +719,9 @@ class OnnxSeq2SeqConfigWithPast(OnnxConfigWithPast):
         flattened_output[f"{name}.{idx}.decoder.value"] = t[1]
         flattened_output[f"{name}.{idx}.encoder.key"] = t[2]
         flattened_output[f"{name}.{idx}.encoder.value"] = t[3]
+
+    def patch_model_for_export(self, model: Union["PreTrainedModel", "TFPreTrainedModel"]) -> ModelPatcher:
+        return Seq2SeqModelPatcher(self, model)
 
 
 class OnnxConfigWithLoss(OnnxConfig, ABC):
