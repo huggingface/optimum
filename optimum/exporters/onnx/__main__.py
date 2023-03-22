1c671ad21721a747c077e66a08469d43059287ac
Félix Marty <9808326+fxmarty@users.noreply.github.com>
2023-03-22 17:08:43 +0100

    use path

--- optimum/exporters/onnx/__main__.py
+++ optimum/exporters/onnx/__main__.py
@@ -405,7 +405,7 @@ def main_export(
 
         # Saving the model config and preprocessor as this is needed sometimes.
         model.config.save_pretrained(output)
-        maybe_save_preprocessors(model, output)
+        maybe_save_preprocessors(model_name_or_path, output)
 
     if task == "stable-diffusion":
         onnx_files_subpaths = [
