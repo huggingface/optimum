577a4522d9de743a7c896d368089b88217b5def7
root <prathik.rao@gmail.com>
2023-04-04 21:18:16 +0000

    remove map

--- examples/onnxruntime/training/image-classification/run_image_classification.py
+++ examples/onnxruntime/training/image-classification/run_image_classification.py
@@ -346,7 +346,7 @@ def main():
                 dataset["train"].shuffle(seed=training_args.seed).select(range(data_args.max_train_samples))
             )
         # Set the training transforms
-        dataset["train"] = dataset["train"].map(train_transforms, batched=True, batch_size=training_args.per_device_train_batch_size)
+        dataset["train"].set_transform(train_transforms)
 
     if training_args.do_eval:
         if "validation" not in dataset:
@@ -356,7 +356,7 @@ def main():
                 dataset["validation"].shuffle(seed=training_args.seed).select(range(data_args.max_eval_samples))
             )
         # Set the validation transforms
-        dataset["validation"] = dataset["validation"].map(val_transforms, batched=True, batch_size=training_args.per_device_eval_batch_size)
+        dataset["validation"].set_transform(val_transforms)
 
     # Initalize our trainer
     trainer = ORTTrainer(
