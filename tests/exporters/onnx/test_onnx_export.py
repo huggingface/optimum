9fc51b67ddb2d6ac25032a1501e1641ecdf7d6b3
Michael Benayoun <mickbenayoun@gmail.com>
2023-05-03 15:33:54 +0200

    Fix tests

--- tests/exporters/onnx/test_onnx_export.py
+++ tests/exporters/onnx/test_onnx_export.py
@@ -308,10 +308,18 @@ class OnnxExportTestCase(TestCase):
 
                 gc.collect()
 
-    def test_all_models_are_tested(self):
+    def test_all_models_tested(self):
         # make sure we test all models
         missing_models_set = TasksManager._SUPPORTED_CLI_MODEL_TYPE - set(PYTORCH_EXPORT_MODELS_TINY.keys())
         if len(missing_models_set) > 0:
+            # TODO: remove that once transformers 4.29.0 is released.
+            import packaging
+            import transformers
+
+            if packaging.version.parse(transformers.__version__) < packaging.version.parse(
+                "4.29.0"
+            ) and missing_models_set == {"sam"}:
+                return
             self.fail(f"Not testing all models. Missing models: {missing_models_set}")
 
     @parameterized.expand(_get_models_to_test(PYTORCH_EXPORT_MODELS_TINY))
