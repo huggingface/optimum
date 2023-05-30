abd63714eb7f63367dcf18b9b29476f39cdbd788
Félix Marty <9808326+fxmarty@users.noreply.github.com>
2023-05-30 17:08:46 +0900

    fix model type detection

--- optimum/exporters/tasks.py
+++ optimum/exporters/tasks.py
@@ -1379,13 +1379,13 @@ class TasksManager:
         model_type = None
         model_class_name = None
         if TasksManager._TASKS_TO_LIBRARY[task.replace("-with-past", "")] == "transformers":
+            config = AutoConfig.from_pretrained(model_name_or_path)
+            model_type = config.model_type.replace("_", "-")
             # TODO: if automatic-speech-recognition is passed as task, it may map to several
             # different auto class (AutoModelForSpeechSeq2Seq or AutoModelForCTC),
             # depending on the model type
             # if original_task in ["auto", "automatic-speech-recognition"]:
             if original_task == "automatic-speech-recognition" or task == "automatic-speech-recognition":
-                config = AutoConfig.from_pretrained(model_name_or_path)
-                model_type = config.model_type.replace("_", "-")
                 if original_task == "auto" and config.architectures is not None:
                     model_class_name = config.architectures[0]
 
