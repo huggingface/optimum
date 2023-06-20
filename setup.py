d921b0429ba17cde0cb3634c3ce98ff15add249a
Ella Charlaix <ella@huggingface.co>
2023-06-20 14:02:01 +0200

    add diffusers extra

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ EXTRAS_REQUIRE = {
     "exporters": ["onnx<1.14.0", "onnxruntime", "timm"],
     "exporters-gpu": ["onnx<1.14.0", "onnxruntime-gpu", "timm"],
     "exporters-tf": ["tensorflow>=2.4,<2.11", "tf2onnx", "onnx", "onnxruntime", "timm", "h5py", "numpy<1.24.0"],
+    "diffusers": ["diffusers"],
     "intel": "optimum-intel",
     "openvino": "optimum-intel[openvino]",
     "nncf": "optimum-intel[nncf]",
