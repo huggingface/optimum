0f09af9327df59968c2d7bf26b6e0134556ef6b1
Ella Charlaix <ella@huggingface.co>
2023-06-22 18:08:10 +0200

    fix style

--- optimum/exporters/onnx/__main__.py
+++ optimum/exporters/onnx/__main__.py
@@ -15,15 +15,23 @@
 """Entry point to the optimum.exporters.onnx command line."""
 
 import argparse
+import os
 from pathlib import Path
 
 from requests.exceptions import ConnectionError as RequestsConnectionError
 from transformers import AutoTokenizer
 from transformers.utils import is_torch_available
-import os
 
 from ...commands.export.onnx import parse_args_onnx
-from ...utils import DEFAULT_DUMMY_SHAPES, logging
+from ...utils import (
+    DEFAULT_DUMMY_SHAPES,
+    DIFFUSION_MODEL_TEXT_ENCODER_SUBFOLDER,
+    DIFFUSION_MODEL_UNET_SUBFOLDER,
+    DIFFUSION_MODEL_VAE_DECODER_SUBFOLDER,
+    DIFFUSION_MODEL_VAE_ENCODER_SUBFOLDER,
+    ONNX_WEIGHTS_NAME,
+    logging,
+)
 from ...utils.save_utils import maybe_save_preprocessors
 from ..error_utils import AtolError, OutputMatchError, ShapeError
 from ..tasks import TasksManager
@@ -35,13 +43,6 @@ from .utils import (
     get_stable_diffusion_models_for_export,
 )
 
-from ...utils import (
-    DIFFUSION_MODEL_TEXT_ENCODER_SUBFOLDER,
-    DIFFUSION_MODEL_UNET_SUBFOLDER,
-    DIFFUSION_MODEL_VAE_DECODER_SUBFOLDER,
-    DIFFUSION_MODEL_VAE_ENCODER_SUBFOLDER,
-    ONNX_WEIGHTS_NAME,
-)
 
 if is_torch_available():
     import torch
@@ -273,7 +274,6 @@ def main_export(
         maybe_save_preprocessors(model_name_or_path, output)
 
     if task == "stable-diffusion":
-
         onnx_files_subpaths = [
             DIFFUSION_MODEL_TEXT_ENCODER_SUBFOLDER,
             DIFFUSION_MODEL_UNET_SUBFOLDER,
