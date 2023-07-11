59d6728ce8b77d5a75380655e238b71634a2928d
Félix Marty <9808326+fxmarty@users.noreply.github.com>
2023-07-11 11:25:42 +0200

    fix if mask is none

--- optimum/bettertransformer/models/attention.py
+++ optimum/bettertransformer/models/attention.py
@@ -74,7 +74,8 @@ def gpt2_wrapped_scaled_dot_product(
 
             # torch.Tensor.expand does no memory copy
             causal_mask = causal_mask.expand(batch_size, -1, -1, -1)
-            attention_mask = causal_mask + attention_mask
+            if attention_mask is not None:
+                attention_mask = causal_mask + attention_mask
 
         sdpa_result = torch.nn.functional.scaled_dot_product_attention(
             query, key, value, attn_mask=attention_mask, dropout_p=dropout_p, is_causal=False
@@ -127,7 +128,8 @@ def gpt_neo_wrapped_scaled_dot_product(
             # torch.Tensor.expand does no memory copy
             causal_mask = causal_mask.expand(batch_size, -1, -1, -1)
 
-        attention_mask = causal_mask + attention_mask
+        if attention_mask is not None:
+            attention_mask = causal_mask + attention_mask
 
         sdpa_result = torch.nn.functional.scaled_dot_product_attention(
             query, key, value, attn_mask=attention_mask, dropout_p=dropout_p, is_causal=False
