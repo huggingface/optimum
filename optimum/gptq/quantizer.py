d272099630fe73f9cfb21064146be67bcde12c95
Marc Sun <marc.sun@hotmail.fr>
2023-07-27 14:29:52 +0000

    add tqdm bar instead

--- optimum/gptq/quantizer.py
+++ optimum/gptq/quantizer.py
@@ -15,6 +15,7 @@
 import copy
 import json
 import os
+from tqdm import tqdm
 from logging import getLogger
 from typing import Any, Dict, List, Optional, Tuple, Union
 
@@ -233,16 +234,10 @@ class GPTQQuantizer(object):
                     - A path to a *directory* containing vocabulary files required by the tokenizer, for instance saved
                       using the [`~PreTrainedTokenizer.save_pretrained`] method, e.g., `./my_model_directory/`.
             verbose (`bool`):
-                Set the logger level to INFO is True
+                Whether to show the tqdm bar or not 
         Returns:
             `nn.Module`: The quantized model
         """
-        if verbose:
-            logger.basicConfig(
-                format="%(asctime)s %(levelname)s [%(name)s] %(message)s",
-                level=logger.INFO,
-                datefmt="%Y-%m-%d %H:%M:%S",
-            )
 
         if not is_auto_gptq_available():
             raise RuntimeError("auto-gptq is required in order to perform quantzation : `pip install auto-gptq`")
@@ -366,7 +361,7 @@ class GPTQQuantizer(object):
 
         # Step 3: Quantize the blocks
         quantizers = {}
-        for i, block in enumerate(blocks):
+        for i, block in enumerate(tqdm(blocks, disable=not verbose, desc = f"Quantizing {self.block_name_to_quantize} blocks ")):
             logger.info(f"Start quantizing block {self.block_name_to_quantize} {i + 1}/{len(blocks)}")
             # move block to cuda if needed
             # in case we have offload modules, we need to put them on cuda because of GPTQ object
@@ -379,7 +374,7 @@ class GPTQQuantizer(object):
             else:
                 layers_name_list = [list(layers.keys())]
             logger.info(f"Module to quantize {layers_name_list}")
-            for subset_name_list in layers_name_list:
+            for subset_name_list in tqdm(layers_name_list, disable=not verbose, leave=False, desc = "Quantizing layers inside the block"):
                 subset_layers = {name: layers[name] for name in subset_name_list}
                 gptq = {}
                 handles = []
