b85d28692e79f5cadc1eabc06a44a078261a60da
Noam Wies <noamwies@gmail.com>
2023-07-30 08:01:06 +0300

    fix formating issues

--- optimum/bettertransformer/models/attention.py
+++ optimum/bettertransformer/models/attention.py
@@ -556,19 +556,23 @@ def llama_forward(
 
     if self.config.pretraining_tp > 1:
         key_value_slicing = (self.num_key_value_heads * self.head_dim) // self.config.pretraining_tp
-        query_slices = self.q_proj.weight.split(
-            (self.num_heads * self.head_dim) // self.config.pretraining_tp, dim=0
-        )
+        query_slices = self.q_proj.weight.split((self.num_heads * self.head_dim) // self.config.pretraining_tp, dim=0)
         key_slices = self.k_proj.weight.split(key_value_slicing, dim=0)
         value_slices = self.v_proj.weight.split(key_value_slicing, dim=0)
 
-        query_states = [torch.nn.functional.linear(hidden_states, query_slices[i]) for i in range(self.config.pretraining_tp)]
+        query_states = [
+            torch.nn.functional.linear(hidden_states, query_slices[i]) for i in range(self.config.pretraining_tp)
+        ]
         query_states = torch.cat(query_states, dim=-1)
 
-        key_states = [torch.nn.functional.linear(hidden_states, key_slices[i]) for i in range(self.config.pretraining_tp)]
+        key_states = [
+            torch.nn.functional.linear(hidden_states, key_slices[i]) for i in range(self.config.pretraining_tp)
+        ]
         key_states = torch.cat(key_states, dim=-1)
 
-        value_states = [torch.nn.functional.linear(hidden_states, value_slices[i]) for i in range(self.config.pretraining_tp)]
+        value_states = [
+            torch.nn.functional.linear(hidden_states, value_slices[i]) for i in range(self.config.pretraining_tp)
+        ]
         value_states = torch.cat(value_states, dim=-1)
 
     else:
@@ -626,7 +630,9 @@ def llama_forward(
     if self.config.pretraining_tp > 1:
         attn_output = attn_output.split(self.hidden_size // self.config.pretraining_tp, dim=2)
         o_proj_slices = self.o_proj.weight.split(self.hidden_size // self.config.pretraining_tp, dim=1)
-        attn_output = sum([torch.nn.functional.linear(attn_output[i], o_proj_slices[i]) for i in range(self.config.pretraining_tp)])
+        attn_output = sum(
+            [torch.nn.functional.linear(attn_output[i], o_proj_slices[i]) for i in range(self.config.pretraining_tp)]
+        )
     else:
         attn_output = self.o_proj(attn_output)
 
