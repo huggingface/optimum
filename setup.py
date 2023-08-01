e84109535c433a4bf9e9319ce7ce052b09f6afbb
Félix Marty <9808326+fxmarty@users.noreply.github.com>
2023-08-01 11:57:44 +0200

    update onnxruntime minimum version

--- setup.py
+++ setup.py
@@ -44,14 +44,14 @@ BENCHMARK_REQUIRE = ["optuna", "tqdm", "scikit-learn", "seqeval", "torchvision",
 EXTRAS_REQUIRE = {
     "onnxruntime": [
         "onnx",
-        "onnxruntime>=1.9.0",
+        "onnxruntime>=1.11.0",
         "datasets>=1.2.1",
         "evaluate",
         "protobuf>=3.20.1",
     ],
     "onnxruntime-gpu": [
         "onnx",
-        "onnxruntime-gpu>=1.9.0",
+        "onnxruntime-gpu>=1.11.0",
         "datasets>=1.2.1",
         "evaluate",
         "protobuf>=3.20.1",
