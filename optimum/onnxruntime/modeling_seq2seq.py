28b1c0e1126b4209d9b8c067dda3186ba4db2b24
Athul Raj K <79792953+krathul@users.noreply.github.com>
2023-08-21 21:21:28 +0530

    Update optimum/onnxruntime/modeling_seq2seq.py
    Co-authored-by: fxmarty <9808326+fxmarty@users.noreply.github.com>

--- optimum/onnxruntime/modeling_seq2seq.py
+++ optimum/onnxruntime/modeling_seq2seq.py
@@ -1477,14 +1477,9 @@ class ORTModelForPix2Struct(ORTModelForConditionalGeneration, GenerationMixin):
             )
 
         # shift inputs to the right
-        if is_torch_fx_proxy(input_ids):
-            # Item assignment is not supported natively for proxies.
-            shifted_input_ids = torch.full(input_ids.shape[:-1] + (1,), decoder_start_token_id)
-            shifted_input_ids = torch.cat([shifted_input_ids, input_ids[..., :-1]], dim=-1)
-        else:
-            shifted_input_ids = input_ids.new_zeros(input_ids.shape)
-            shifted_input_ids[..., 1:] = input_ids[..., :-1].clone()
-            shifted_input_ids[..., 0] = decoder_start_token_id
+        shifted_input_ids = input_ids.new_zeros(input_ids.shape)
+        shifted_input_ids[..., 1:] = input_ids[..., :-1].clone()
+        shifted_input_ids[..., 0] = decoder_start_token_id
 
         if pad_token_id is None:
             raise ValueError("self.model.config.pad_token_id has to be defined.")
