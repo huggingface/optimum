c9711b8fd3c7b4e43851d42ebaabb34dcd9473da
Issam Arabi <issam@cs.toronto.edu>
2023-10-01 17:26:57 -0400

    Add test for DETR BetterTransformer
    Signed-off-by: Issam Arabi <issam@cs.toronto.edu>

--- tests/bettertransformer/test_vision.py
+++ tests/bettertransformer/test_vision.py
@@ -27,7 +27,7 @@ class BetterTransformersVisionTest(BetterTransformersTestMixin, unittest.TestCas
     r"""
     Testing suite for Vision Models - tests all the tests defined in `BetterTransformersTestMixin`
     """
-    SUPPORTED_ARCH = ["blip-2", "clip", "clip_text_model", "deit", "vilt", "vit", "vit_mae", "vit_msn", "yolos"]
+    SUPPORTED_ARCH = ["blip-2", "clip", "clip_text_model", "deit", "detr", "vilt", "vit", "vit_mae", "vit_msn", "yolos"]
 
     def prepare_inputs_for_class(self, model_id, model_type, batch_size=3, **preprocessor_kwargs):
         if model_type == "vilt":
@@ -56,6 +56,14 @@ class BetterTransformersVisionTest(BetterTransformersTestMixin, unittest.TestCas
 
             if model_type == "blip-2":
                 inputs["decoder_input_ids"] = inputs["input_ids"]
+        
+        elif model_type == "detr":
+            # Assuming detr just needs an image
+            url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+            image = Image.open(requests.get(url, stream=True).raw)
+
+            feature_extractor = AutoFeatureExtractor.from_pretrained("hf-internal-testing/tiny-random-DetrModel")
+            inputs = feature_extractor(images=image, return_tensors="pt")
 
         else:
             url = "http://images.cocodataset.org/val2017/000000039769.jpg"
