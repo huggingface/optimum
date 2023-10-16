eb9f110bf0833f8bec1a09e4317fece452c608aa
Félix Marty <9808326+fxmarty@users.noreply.github.com>
2023-10-16 16:32:20 +0200

    disable bart onnx export for text-classification and question-answering

--- tests/onnxruntime/test_quantization.py
+++ tests/onnxruntime/test_quantization.py
@@ -76,7 +76,6 @@ class ORTDynamicQuantizationTest(unittest.TestCase):
         (ORTModelForSequenceClassification, "hf-internal-testing/tiny-random-bert", 30),
         (ORTModelForSequenceClassification, "hf-internal-testing/tiny-random-roberta", 30),
         (ORTModelForSequenceClassification, "hf-internal-testing/tiny-random-distilbert", 30),
-        (ORTModelForSequenceClassification, "hf-internal-testing/tiny-random-bart", 32),
     )
 
     SUPPORTED_DECODER_ARCHITECTURES_WITH_EXPECTED_QUANTIZED_MATMULS = (
