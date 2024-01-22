917255f27b55bb072cf947753213b37e5e547891
Ella Charlaix <ella@huggingface.co>
2024-01-22 19:21:41 +0100

    remove

--- optimum/exporters/onnx/__main__.py
+++ optimum/exporters/onnx/__main__.py
@@ -480,7 +480,6 @@ def onnx_export(
     **kwargs_shapes,
 ):
     library_name = TasksManager._infer_library_from_model(model)
-    # framework = "pt" if is_torch_available() and isinstance(model, torch.nn.Module) else "tf"
     dtype = model.dtype if library_name in {"transformers", "diffusers"} else model.config.torch_dtype
     float_dtype = "fp16" if "float16" in str(dtype) else "fp32"
     model_type = "stable-diffusion" if library_name == "diffusers" else model.config.model_type.replace("_", "-")
