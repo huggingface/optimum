5b4b869492226cd17db87a1a1966ab7cd6bd0bd4
Ella Charlaix <ella@huggingface.co>
2024-01-22 16:08:58 +0100

    fix

--- optimum/exporters/tasks.py
+++ optimum/exporters/tasks.py
@@ -1449,9 +1449,7 @@ class TasksManager:
             if auto_cls is None:
                 continue
             model_mapping = auto_cls._model_mapping._model_mapping
-            flatten = lambda val: sum(map(flatten, val), []) if isinstance(val, (tuple, list)) else [val.lower()]
-            # Lowercase to avoid mismatch between MptForCausalLM and MPTForCausalLM
-            if target_name.lower() in flatten(list(model_mapping.values())):
+            if target_name in model_mapping.values():
                 task_name = task
                 break
         if task_name is None:
