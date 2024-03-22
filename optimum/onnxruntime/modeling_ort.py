760c947af5f4378abd98b787c046a3d097796f96
Ella Charlaix <ella@huggingface.co>
2024-03-22 12:31:38 +0100

    Add can_generate method to fix compatibility with transformers v4.39.0

--- optimum/onnxruntime/modeling_ort.py
+++ optimum/onnxruntime/modeling_ort.py
@@ -52,6 +52,7 @@ from transformers.modeling_outputs import (
     SequenceClassifierOutput,
     TokenClassifierOutput,
     XVectorOutput,
+    GenerationMixin,
 )
 
 import onnxruntime as ort
@@ -893,6 +894,12 @@ class ORTModel(OptimizedModel):
             preprocessors = maybe_load_preprocessors(model_path.as_posix(), subfolder=subfolder)
 
         return model_cache_path, preprocessors
+
+    def can_generate(self) -> bool:
+        """
+        Returns whether this model can generate sequences with `.generate()`.
+        """
+        return isinstance(self, GenerationMixin)
 
 
 FEATURE_EXTRACTION_EXAMPLE = r"""
