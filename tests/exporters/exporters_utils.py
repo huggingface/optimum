d34763121b0be2f63cbbc1fba98bbeedc1a48273
Joe Obarzanek <pogzyb@umich.edu>
2024-04-01 08:12:01 -0400

    Use hf-internal model

--- tests/exporters/exporters_utils.py
+++ tests/exporters/exporters_utils.py
@@ -237,7 +237,7 @@ PYTORCH_EXPORT_MODELS_LARGE = {
     # "longformer": "allenai/longformer-base-4096",
     "m2m-100": "hf-internal-testing/tiny-random-m2m_100",  # Not using facebook/m2m100_418M because it takes too much time for testing.
     "marian": "Helsinki-NLP/opus-mt-en-de",
-    "markuplm": "microsoft/markuplm-base",
+    "markuplm": "hf-internal-testing/tiny-random-MarkupLMModel",
     "mbart": "sshleifer/tiny-mbart",
     "mobilebert": "google/mobilebert-uncased",
     # "mobilenet_v1": "google/mobilenet_v1_0.75_192",
