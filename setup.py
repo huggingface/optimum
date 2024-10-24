72ca53c0ce1e2a934a14f8c1b8a154e12296a5d7
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2024-10-24 10:51:40 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -15,8 +15,7 @@ except Exception as error:
 REQUIRED_PKGS = [
     "coloredlogs",
     "sympy",
-    "transformers @ git+https://github.com/huggingface/transformers.git",
-    # "transformers[sentencepiece]>=4.29",
+    "transformers>=4.29",
     "torch>=1.11",
     "packaging",
     "numpy",
