7aa1562bf5614a1d71736f61ab42f4eb121dd815
Michael Benayoun <mickbenayoun@gmail.com>
2024-12-12 17:20:12 +0100

    Test with different instance

--- tests/executorch/export/test_exporters_executorch.py
+++ tests/executorch/export/test_exporters_executorch.py
@@ -74,7 +74,6 @@ class TestExportToExecuTorchCLI(unittest.TestCase):
 
     @slow
     @pytest.mark.run_slow
-    @pytest.mark.skip
     def test_gemma2_export_to_executorch(self):
         model_id = "unsloth/gemma-2-2b-it"
         task = "text-generation"
@@ -89,7 +88,6 @@ class TestExportToExecuTorchCLI(unittest.TestCase):
 
     @slow
     @pytest.mark.run_slow
-    @pytest.mark.skip
     def test_gemma_export_to_executorch(self):
         model_id = "weqweasdas/RM-Gemma-2B"
         task = "text-generation"
