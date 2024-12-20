a0afb915aa9b33f0ddfdd13c0d76fd05d06511a0
Jingya <jingya@huggingface.co>
2024-12-20 15:47:51 +0000

    fix style

--- optimum/exporters/tasks.py
+++ optimum/exporters/tasks.py
@@ -1790,7 +1790,7 @@ class TasksManager:
                 cache_dir=cache_dir,
                 token=token,
             )
-        elif type(model) == type:
+        elif isinstance(model, type):
             inferred_task_name = cls._infer_task_from_model_or_model_class(model_class=model)
         else:
             inferred_task_name = cls._infer_task_from_model_or_model_class(model=model)
@@ -1944,7 +1944,7 @@ class TasksManager:
                 cache_dir=cache_dir,
                 token=token,
             )
-        elif type(model) == type:
+        elif isinstance(model, type):
             library_name = cls._infer_library_from_model_or_model_class(model_class=model)
         else:
             library_name = cls._infer_library_from_model_or_model_class(model=model)
