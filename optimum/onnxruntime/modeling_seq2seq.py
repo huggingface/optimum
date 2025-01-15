e9abe6ae90c10cf83d05c4ebe2a8c1f3d6e25798
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2025-01-15 14:59:14 +0100

    typo

--- optimum/onnxruntime/modeling_seq2seq.py
+++ optimum/onnxruntime/modeling_seq2seq.py
@@ -433,7 +433,7 @@ class ORTEncoderForVisionEncoderDecoder(ORTEncoder):
         else:
             onnx_inputs = self._prepare_onnx_inputs(use_torch, **model_inputs)
             onnx_outputs = self.session.run(None, onnx_inputs)
-            model_outputs = self._prepare_model_outputs(use_torch, *onnx_outputs)
+            model_outputs = self._prepare_onnx_outputs(use_torch, *onnx_outputs)
 
             last_hidden_state = model_outputs["last_hidden_state"]
 
@@ -475,7 +475,7 @@ class ORTEncoderForPix2Struct(ORTEncoder):
         else:
             onnx_inputs = self._prepare_onnx_inputs(use_torch, **model_inputs)
             onnx_outputs = self.session.run(None, onnx_inputs)
-            model_outputs = self._prepare_model_outputs(use_torch, *onnx_outputs)
+            model_outputs = self._prepare_onnx_outputs(use_torch, *onnx_outputs)
 
             last_hidden_state = model_outputs["last_hidden_state"]
 
