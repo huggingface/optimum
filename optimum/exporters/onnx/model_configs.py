cccc05ce3f5517c1c76f4dabb2e83c6bdea47124
ariG23498 <aritra.born2fly@gmail.com>
2025-02-10 16:31:04 +0530

    building dummy inputs for vit post

--- optimum/exporters/onnx/model_configs.py
+++ optimum/exporters/onnx/model_configs.py
@@ -774,8 +774,48 @@ class ViTOnnxConfig(VisionOnnxConfig):
         return common_outputs
 
 
+class VitPoseDummyInputGenerator(DummyVisionInputGenerator):
+    def __init__(
+        self,
+        task: str,
+        normalized_config: NormalizedVisionConfig,
+        batch_size: int = DEFAULT_DUMMY_SHAPES["batch_size"],
+        num_channels: int = DEFAULT_DUMMY_SHAPES["num_channels"],
+        width: int = DEFAULT_DUMMY_SHAPES["width"],
+        height: int = DEFAULT_DUMMY_SHAPES["height"],
+        **kwargs,
+    ):
+        super().__init__(
+            task=task,
+            normalized_config=normalized_config,
+            batch_size=batch_size,
+            num_channels=num_channels,
+            width=width,
+            height=height,
+            **kwargs,
+        )
+
+        from transformers.onnx.utils import get_preprocessor
+
+        preprocessor = get_preprocessor(normalized_config._name_or_path)
+        if preprocessor is not None and hasattr(preprocessor, "size"):
+            self.height = preprocessor.size.get("height", self.height)
+            self.width = preprocessor.size.get("width", self.width)
+
+    def generate(self, input_name: str, framework: str = "pt", int_dtype: str = "int64", float_dtype: str = "fp32"):
+        input_ = super().generate(
+            input_name=input_name, framework=framework, int_dtype=int_dtype, float_dtype=float_dtype
+        )
+        return input_
+
+
 class VitPoseOnnxConfig(ViTOnnxConfig):
-    pass
+    DUMMY_INPUT_GENERATOR_CLASSES = (VitPoseDummyInputGenerator, )
+    ATOL_FOR_VALIDATION = 1e-4
+
+    @property
+    def inputs(self) -> Dict[str, Dict[int, str]]:
+        return {"pixel_values": {0: "batch_size"}}
 
 
 class CvTOnnxConfig(ViTOnnxConfig):
