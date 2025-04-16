7b7ad442609e97abe621fe9c596ef561df4d4eab
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2025-04-16 11:46:18 +0200

    first fixes of doctest

--- tests/run_doctest.sh
+++ tests/run_doctest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-pip install accelerate
-pip install .[benchmark]
-touch optimum/__init__.py
+# we use 4.48 for compatibility with bettertransformer
+pip install .[tests] transformers==4.48.* optuna
 python tests/utils/prepare_for_doc_test.py optimum docs
 pytest --verbose -s --doctest-modules $(cat tests/utils/documentation_tests.txt) --doctest-continue-on-failure --doctest-glob='*.mdx'
+python tests/utils/prepare_for_doc_test.py optimum docs --remove_new_line
