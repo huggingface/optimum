a093dcce6fda1f641ec879d2ba3e10aae50d4862
Ella Charlaix <ella@huggingface.co>
2025-05-15 16:11:43 +0200

    compatibility with transformers 4.52

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ except Exception as error:
 
 
 REQUIRED_PKGS = [
-    "transformers>=4.29",
+    # "transformers>=4.29",
+    "transformers @ git+https://github.com/huggingface/transformers.git",
     "torch>=1.11",
     "packaging",
     "numpy",
@@ -50,14 +51,14 @@ EXTRAS_REQUIRE = {
         "datasets>=1.2.1",
         "protobuf>=3.20.1",
         "onnxruntime>=1.11.0",
-        "transformers>=4.36,<4.52.0",
+        "transformers>=4.36,<4.53.0",
     ],
     "onnxruntime-gpu": [
         "onnx",
         "datasets>=1.2.1",
         "protobuf>=3.20.1",
         "onnxruntime-gpu>=1.11.0",
-        "transformers>=4.36,<4.52.0",
+        "transformers>=4.36,<4.53.0",
     ],
     "onnxruntime-training": [
         "evaluate",
@@ -65,7 +66,7 @@ EXTRAS_REQUIRE = {
         "accelerate",
         "datasets>=1.2.1",
         "protobuf>=3.20.1",
-        "transformers>=4.36,<4.52.0",
+        "transformers>=4.36,<4.53.0",
         "onnxruntime-training>=1.11.0",
     ],
     "exporters": [
@@ -73,14 +74,14 @@ EXTRAS_REQUIRE = {
         "timm",
         "onnxruntime",
         "protobuf>=3.20.1",
-        "transformers>=4.36,<4.52.0",
+        "transformers>=4.36,<4.53.0",
     ],
     "exporters-gpu": [
         "onnx",
         "timm",
         "onnxruntime-gpu",
         "protobuf>=3.20.1",
-        "transformers>=4.36,<4.52.0",
+        "transformers>=4.36,<4.53.0",
     ],
     "exporters-tf": [
         "onnx",
