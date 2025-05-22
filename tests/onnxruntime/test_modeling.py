046d0f134cd3403769d97ba245ec865aeecfca66
Ella Charlaix <ella@huggingface.co>
2025-05-22 15:12:51 +0200

    add min max tokens pipeline test

--- tests/onnxruntime/test_modeling.py
+++ tests/onnxruntime/test_modeling.py
@@ -2404,7 +2404,7 @@ class ORTModelForCausalLMIntegrationTest(ORTModelTestMixin):
         tokenizer = get_preprocessor(model_id)
         pipe = pipeline("text-generation", model=onnx_model, tokenizer=tokenizer)
         text = "My Name is Philipp and i live"
-        outputs = pipe(text)
+        outputs = pipe(text, min_new_tokens=10, max_new_tokens=10)
 
         self.assertEqual(pipe.device, onnx_model.device)
         self.assertIsInstance(outputs[0]["generated_text"], str)
@@ -2420,7 +2420,7 @@ class ORTModelForCausalLMIntegrationTest(ORTModelTestMixin):
                     model_kwargs=model_kwargs,
                     accelerator="ort",
                 )
-                outputs_local_model = pipe(text)
+                outputs_local_model = pipe(text, min_new_tokens=10, max_new_tokens=10)
                 self.assertEqual(outputs[0]["generated_text"], outputs_local_model[0]["generated_text"])
 
         gc.collect()
