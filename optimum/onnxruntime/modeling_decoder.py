5f81515ef476664990735c78623b494d2c88ebea
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2025-05-25 10:00:04 +0200

    remove unnecessary

--- optimum/onnxruntime/modeling_decoder.py
+++ optimum/onnxruntime/modeling_decoder.py
@@ -394,13 +394,6 @@ class ORTModelForCausalLM(ORTModel, GenerationMixin):
                 remove_prefix_length = input_ids.shape[1] - 1
             input_ids = input_ids[:, remove_prefix_length:]
 
-        # if attention_mask is not None and position_ids is None:
-        #     # create position_ids on the fly for batch generation
-        #     position_ids = attention_mask.long().cumsum(-1) - 1
-        #     position_ids.masked_fill_(attention_mask == 0, 1)
-        #     if past_key_values is not None:
-        #         position_ids = position_ids[:, -1].unsqueeze(-1)
-
         return {
             "input_ids": input_ids,
             "attention_mask": attention_mask,
