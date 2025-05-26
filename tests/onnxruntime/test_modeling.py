dddf3d363ac63fa17094ec52e40dc2ffad8b90a4
Ella Charlaix <ella@huggingface.co>
2025-05-26 11:02:36 +0200

    update model

--- tests/onnxruntime/test_modeling.py
+++ tests/onnxruntime/test_modeling.py
@@ -930,8 +930,8 @@ class ORTModelIntegrationTest(unittest.TestCase):
             os.environ.pop("FORCE_ONNX_EXTERNAL_DATA")
 
     @pytest.mark.run_in_series
-    def _test_trust_remote_code(self):
-        model_id = "fxmarty/tiny-testing-gpt2-remote-code"
+    def test_trust_remote_code(self):
+        model_id = "optimum-internal-testing/tiny-testing-gpt2-remote-code"
         ort_model = ORTModelForCausalLM.from_pretrained(model_id, export=True, trust_remote_code=True)
         pt_model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True)
 
