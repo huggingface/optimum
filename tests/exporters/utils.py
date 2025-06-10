55e80ea1a3df53271a573d8c032bc6f6db53cccf
Ella Charlaix <ella@huggingface.co>
2025-06-10 17:01:55 +0200

    fix

--- tests/exporters/utils.py
+++ tests/exporters/utils.py
@@ -57,7 +57,7 @@ PYTORCH_EXPORT_MODELS_TINY = {
     "blenderbot": "hf-internal-testing/tiny-random-BlenderbotModel",
     "bloom": "hf-internal-testing/tiny-random-BloomModel",
     "camembert": "hf-internal-testing/tiny-random-camembert",
-    "chinese_clip": "hf-internal-testing/tiny-random-ChineseCLIPModel",
+    "chinese-clip": "hf-internal-testing/tiny-random-ChineseCLIPModel",
     "clip": "hf-internal-testing/tiny-random-CLIPModel",
     "clip-vision-model": "fxmarty/clip-vision-model-tiny",
     "colpali": "hf-internal-testing/tiny-random-ColPaliForRetrieval",
